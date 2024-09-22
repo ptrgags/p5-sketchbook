c3e6afcce213c3220a1cc522cd1b69087e9a2be6
Peter Gagliardi <ptrgags@gmail.com>
2024-09-22 19:51:42 -0400

    Use PhyllotaxisPalette in the PhyllotaxisPalette Sketch

--- PhyllotaxisPalette/PhyllotaxisPalette.js
+++ PhyllotaxisPalette/PhyllotaxisPalette.js
@@ -1,11 +1,11 @@
-const GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2;
-const GOLDEN_ANGLE = (2 * Math.PI) / GOLDEN_RATIO / GOLDEN_RATIO;
+import { PhyllotaxisPalette } from "../sketchlib/PhyllotaxisPalette.js";
 
 const PRIMORDIUM_CREATION_PERIOD = 50;
 const RECT_SIZE = 8;
 
 export const sketch = (p) => {
-  let primordia_count = 0;
+  let primordia_count = 2;
+  let palette = new PhyllotaxisPalette(primordia_count);
   p.setup = () => {
     p.createCanvas(500, 700);
   };
@@ -15,32 +15,35 @@ export const sketch = (p) => {
 
     p.push();
     p.colorMode(p.HSB, 1.0);
-    p.translate(p.width / 2, p.height / 2);
-    const max_radius = p.width / 2;
-    for (let i = 0; i <= primordia_count; i++) {
-      const radius = 1.0 - i / primordia_count;
-      const angle = i * GOLDEN_ANGLE;
 
-      p.fill((angle % p.TWO_PI) / p.TWO_PI, radius, 1.0);
+    const max_radius = p.width / 2;
+    for (let i = 0; i < primordia_count; i++) {
+      const { hue, saturation, value } = palette.get_color(i);
+      p.fill(hue, saturation, value);
+
+      p.push();
+      p.translate(p.width / 2, p.height / 2);
+      const { r, theta } = palette.get_point(i);
       p.circle(
-        max_radius * radius * p.cos(angle),
-        -max_radius * radius * p.sin(angle),
+        max_radius * r * p.cos(theta),
+        -max_radius * r * p.sin(theta),
         20
       );
+      p.pop();
 
-      p.rect(
-        -p.width / 2.0,
-        -p.height / 2 + i * RECT_SIZE,
-        RECT_SIZE,
-        RECT_SIZE
-      );
+      p.rect(0, i * RECT_SIZE, RECT_SIZE, RECT_SIZE);
     }
 
     p.pop();
 
+    // Add another point to the palette over time.
     if (p.frameCount % PRIMORDIUM_CREATION_PERIOD == 0) {
+      const prev_count = primordia_count;
       primordia_count++;
       primordia_count = p.min(primordia_count, p.height / RECT_SIZE);
+      if (primordia_count != prev_count) {
+        palette = new PhyllotaxisPalette(primordia_count);
+      }
     }
   };
 };
