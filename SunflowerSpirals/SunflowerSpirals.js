3163c0a66d3537d06e29f9273288845d8bd3b080
Peter Gagliardi <ptrgags@gmail.com>
2024-09-23 19:49:33 -0400

    Simplify primorida rendering math

--- SunflowerSpirals/SunflowerSpirals.js
+++ SunflowerSpirals/SunflowerSpirals.js
@@ -25,6 +25,13 @@ class Primordium {
   }
 }
 
+function to_rect(r, theta) {
+  return {
+    x: r * Math.cos(theta),
+    y: r * -Math.sin(theta),
+  };
+}
+
 export const sketch = (p) => {
   const primordia = [];
   let primordia_count = 0;
@@ -46,61 +53,28 @@ export const sketch = (p) => {
       p.push();
       p.translate(p.width / 2, p.height / 2);
 
-      // draw a line from the origin to the primrodium
-      const start = {
-        r: primordium.distance - 2 * primordium.size,
-        theta: primordium.angle,
-      };
-
-      const dt = 0.2;
-
-      const side1 = {
-        r: primordium.distance - primordium.size,
-        theta: primordium.angle - dt,
-      };
-
-      const side2 = {
-        r: primordium.distance - primordium.size,
-        theta: primordium.angle + dt,
-      };
-
-      const tip = {
-        r: primordium.distance + primordium.size,
-        theta: primordium.angle,
-      };
+      const r = primordium.distance;
+      const size = primordium.size;
+      const theta = primordium.angle;
 
       const control_offset = 0.75;
-      const control1 = {
-        r: side1.r + control_offset * primordium.size,
-        theta: side1.theta,
-      };
+      const r_start = r - 2 * size;
+      const r_sides = r - size;
+      const r_control_points = r_sides + control_offset * size;
+      const r_tip = r + size;
+      const r_tip_control_point = r_tip - control_offset * size;
 
-      const control2 = {
-        r: side2.r + control_offset * primordium.size,
-        theta: side2.theta,
-      };
+      const HALF_ANGULAR_SIZE = 0.2;
+      const theta1 = theta - HALF_ANGULAR_SIZE;
+      const theta2 = theta + HALF_ANGULAR_SIZE;
 
-      const control_tip = {
-        r: tip.r - control_offset * primordium.size,
-        theta: tip.theta,
-      };
-
-      for (const point of [
-        start,
-        side1,
-        side2,
-        tip,
-        control1,
-        control2,
-        control_tip,
-      ]) {
-        point.x = point.r * p.cos(point.theta);
-        point.y = point.r * -p.sin(point.theta);
-      }
-
-      //p.line(start.x, start.y, side1.x, side1.y);
-      //p.line(side1.x, side1.y, control1.x, control1.y);
-      //p.line(control1.x, control1.y, control_tip.x, control_tip.y);
+      const start = to_rect(r_start, theta);
+      const side1 = to_rect(r_sides, theta1);
+      const side2 = to_rect(r_sides, theta2);
+      const control1 = to_rect(r_control_points, theta1);
+      const control2 = to_rect(r_control_points, theta2);
+      const control_tip = to_rect(r_tip_control_point, theta);
+      const tip = to_rect(r_tip, theta);
 
       p.beginShape();
       p.vertex(start.x, start.y);
@@ -121,45 +95,7 @@ export const sketch = (p) => {
         side2.x,
         side2.y
       );
-      /*p.vertex(side1.x, side1.y);
-      p.vertex(control_tip.x, control_tip.y);
-      p.vertex(tip.x, tip.y);*/
-      //p.vertex(control_tip.x, control_tip.y);
-      //p.vertex(control2.x, control2.y);
-      //p.vertex(side2.x, side2.y);
       p.endShape(p.CLOSE);
-
-      /*p.beginShape();
-      p.vertex(start.x, start.y);
-      p.vertex(side1.x, side1.y);
-      p.vertex(tip.x, tip.y);*/
-      /*
-      p.bezierVertex(
-        control1.x,
-        control1.y,
-        control_tip.x,
-        control_tip.y,
-        tip.x,
-        tip.y
-      );
-      p.bezierVertex(
-        control_tip.x,
-        control_tip.y,
-        control2.x,
-        control2.y,
-        side2.x,
-        side2.x
-      );
-      */
-      //p.vertex(side2.x, side2.y);
-      //p.endShape(p.CLOSE);
-
-      /*
-      p.circle(
-        primordium.distance * p.cos(primordium.angle),
-        -primordium.distance * p.sin(primordium.angle),
-        primordium.size
-      );*/
       p.pop();
     }
 
