1ccf699b098cf6b88358173455ad0d15d044e965
Peter Gagliardi <ptrgags@gmail.com>
2024-12-03 17:26:15 -0500

    Try animating the points

--- StringArt/StringArt.js
+++ StringArt/StringArt.js
@@ -56,6 +56,7 @@ export const sketch = (p) => {
     p.translate(cx, cy);
 
     // Draw the unit circle
+    // Colors from https://coolors.co/palette/540d6e-ee4266-ffd23f-3bceac-0ead69
     p.stroke("#540D6E");
     p.strokeWeight(5);
     p.noFill();
@@ -77,6 +78,28 @@ export const sketch = (p) => {
       } else {
         p.line(start_polar.x, start_polar.y, end_polar.x, end_polar.y);
       }
+    }
+
+    // Animate transitions between points
+    p.noStroke();
+    p.fill("#3BCEAC");
+    const ANIMATION_PERIOD = 300;
+    const t = (p.frameCount % ANIMATION_PERIOD) / ANIMATION_PERIOD;
+    for (let i = 0; i < MODULUS; i++) {
+      const start = i;
+      const end = poly.compute(i);
+
+      if (start == end) {
+        continue;
+      }
+
+      const start_polar = points[start];
+      const end_polar = points[end];
+
+      const x = p.lerp(start_polar.x, end_polar.x, t);
+      const y = p.lerp(start_polar.y, end_polar.y, t);
+
+      p.circle(x, y, 0.02 * p.width);
     }
 
     // Draw dots at each point on the circle
