c776cefbe3020934d5811e78ffa67e07a9f9f4b9
Peter Gagliardi <ptrgags@gmail.com>
2025-02-04 20:49:30 -0500

    Add a function to compute evenly-spaced perspective rectangles

--- PerspectiveRailroad/PerspectiveRailroad.js
+++ PerspectiveRailroad/PerspectiveRailroad.js
@@ -51,6 +51,66 @@ function draw_line_segment(p, a, b) {
 const RAIL_WIDTH = 30;
 const RAIL_HEIGHT = 50;
 
+const MAX_ITERATIONS = 500;
+
+/**
+ * Get a number of evenly spaced rectangles along the ground in 1-point
+ * perspective. This will look like a sidwalk.
+ * @param {Point} point_a The bottom left corner of the sidewalk
+ * @param {Point} point_b The bottom right corner of the sidewalk. It must be at the same y-value as point_a.
+ * @param {Point} vp Vanishing point where rectangles converge
+ * @param {number1} vertical_spacing vertical spacing in px between the first two lines.
+ * @returns {object} An object with data representing the lines and points of the pattern.
+ */
+function even_spaced_rectangles(point_a, point_b, vp, vertical_spacing) {
+  // the two points determine the bottom line,
+  // the second line is the given number of pixels above it.
+  const first_line = point_a.join(point_b);
+  const second_line = point_a
+    .add(Point.DIR_Y.scale(vertical_spacing))
+    .join(Point.DIR_X);
+
+  // Left and right guidelines that lead to the vanishing point
+  const guide_left = point_a.join(vp);
+  const guide_right = point_b.join(vp);
+
+  // Define the first rectangle explicitly, the rest will be computed
+  // by perspective construction
+  const horizontal_lines = [first_line, second_line];
+  const left_endpoints = [point_a, second_line.meet(guide_left)];
+  const right_endpoints = [point_b, second_line.meet(guide_right)];
+
+  // Find the center of the rectangle, then draw a line through it and the
+  // vanishing point.
+  const diag1 = left_endpoints[1].join(point_b);
+  const diag2 = right_endpoints[1].join(point_a);
+  const center = diag1.meet(diag2);
+  const center_line = center.join(vp);
+
+  for (let i = 0; i < MAX_ITERATIONS; i++) {
+    const prev_left = left_endpoints[left_endpoints.length - 2];
+    const current_line = horizontal_lines[horizontal_lines.length - 1];
+    const center = current_line.meet(center_line);
+    const line_to_next = prev_left.join(center);
+    const next_right = line_to_next.meet(guide_right);
+    const next_line = next_right.join(Point.DIR_X);
+    const next_left = next_line.meet(guide_left);
+
+    horizontal_lines.push(next_line);
+    left_endpoints.push(next_left);
+    right_endpoints.push(next_right);
+  }
+
+  return {
+    guide_left,
+    guide_right,
+    horizontal_lines,
+    left_endpoints,
+    right_endpoints,
+    vanishing_point: vp,
+  };
+}
+
 export const sketch = (p) => {
   let canvas;
   p.setup = () => {
@@ -89,29 +149,24 @@ export const sketch = (p) => {
     draw_line_segment(p, VP_RAILS, isx_B_top_right);
 
     const tie_bottom_left = Point.point(50, 0);
-    const first_tie_top = tie_bottom_left
-      .add(Point.DIR_Y.scale(30))
-      .join(Point.DIR_X);
-    const ties_left = tie_bottom_left.join(VP_RAILS);
-
     const tie_bottom_right = Point.point(475, 0);
-    const ties_right = tie_bottom_right.join(VP_RAILS);
-    const tie_top_left = ties_left.meet(first_tie_top);
-    const tie_top_right = ties_right.meet(first_tie_top);
 
-    const tie_diag1 = tie_top_left.join(tie_bottom_right);
-    const tie_diag2 = tie_top_right.join(tie_bottom_left);
-    const tie_center = tie_diag1.meet(tie_diag2);
-    const rail_center = tie_center.join(VP_RAILS);
+    const TIE_SPACING = 100;
+    const tie_bottoms = even_spaced_rectangles(
+      tie_bottom_left,
+      tie_bottom_right,
+      VP_RAILS,
+      TIE_SPACING
+    );
 
     // debug lines
     p.stroke(255, 0, 0);
-    draw_line(p, ties_left);
-    draw_line(p, first_tie_top);
-    draw_line(p, ties_right);
-    draw_line(p, tie_diag1);
-    draw_line(p, tie_diag2);
-    draw_line(p, rail_center);
+    draw_line(p, tie_bottoms.guide_left);
+    draw_line(p, tie_bottoms.guide_right);
+    tie_bottoms.left_endpoints.forEach((left, i) => {
+      const right = tie_bottoms.right_endpoints[i];
+      draw_line_segment(p, left, right);
+    });
 
     p.stroke(0);
 
