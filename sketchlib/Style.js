518af6aae1d5d596ede07e1edcfba6d42275ddd6
Peter Gagliardi <ptrgags@gmail.com>
2025-03-10 11:43:56 -0400

    Allow CSS colors in Color

--- sketchlib/Style.js
+++ sketchlib/Style.js
@@ -1,3 +1,18 @@
+const REGEX_HEX_COLOR = /^#?[0-9A-Fa-f]{6}$/;
+
+/**
+ * Format a u8 as a 2-digit hex number with leading 0s as needed
+ * @param {number} value_u8
+ */
+function format_hex_u8(value_u8) {
+  const hex_value = value_u8.toString(16);
+  if (hex_value.length === 1) {
+    return `0${hex_value}`;
+  }
+
+  return hex_value;
+}
+
 export class Color {
   /**
    * Constructor
@@ -9,6 +24,40 @@ export class Color {
     this.r = r;
     this.g = g;
     this.b = b;
+  }
+
+  /**
+   * Convert a color to a CSS color code
+   * @returns {string} A string in the form #RRGGBB
+   */
+  to_hex_code() {
+    const r = format_hex_u8(this.r);
+    const g = format_hex_u8(this.g);
+    const b = format_hex_u8(this.b);
+    return `#${r}${g}${b}`;
+  }
+
+  /**
+   * Parse a color from a hex code like RRGGBB or #RRGGBB. This is useful
+   * when the input came from an HTML color input
+   * @param {string} hex_code The hex code
+   * @returns {Color} The parsed color
+   */
+  static from_hex_code(hex_code) {
+    if (!REGEX_HEX_COLOR.test(hex_code)) {
+      throw new Error("hex_code must be in the form RRGGBB or #RRGGBB");
+    }
+
+    const start = hex_code.charAt(0) === "#" ? 1 : 0;
+    const red_string = hex_code.slice(start, start + 2);
+    const green_string = hex_code.slice(start + 2, start + 4);
+    const blue_string = hex_code.slice(start + 4);
+
+    const red = parseInt(red_string, 16);
+    const green = parseInt(green_string, 16);
+    const blue = parseInt(blue_string, 16);
+
+    return new Color(red, green, blue);
   }
 }
 Color.BLACK = Object.freeze(new Color(0, 0, 0));
---
+++ sketchlib/Style.test.js
@@ -0,0 +1,37 @@
+import { describe, it, expect } from "vitest";
+import { Color } from "./Style";
+
+describe("Color", () => {
+  it("from_hex_code with invalid hex code throws", () => {
+    expect(() => {
+      return Color.from_hex_code("nope");
+    }).toThrowError("must be in the form");
+  });
+
+  it("to_hex_code formats as CSS color", () => {
+    const color = new Color(3, 45, 255);
+
+    const result = color.to_hex_code();
+
+    const expected = "#032dff";
+    expect(result).toEqual(expected);
+  });
+
+  it("from_hex_code parses CSS color", () => {
+    const hex_code = "#0fab35";
+
+    const result = Color.from_hex_code(hex_code);
+
+    const expected = new Color(0x0f, 0xab, 0x35);
+    expect(result).toEqual(expected);
+  });
+
+  it("from_hex_code parses color without hashtag", () => {
+    const hex_code = "0fab35";
+
+    const result = Color.from_hex_code(hex_code);
+
+    const expected = new Color(0x0f, 0xab, 0x35);
+    expect(result).toEqual(expected);
+  });
+});
