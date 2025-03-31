effc98926b8e1508d8e3ba28d1bf2263ca625a2d
Peter Gagliardi <ptrgags@gmail.com>
2025-03-30 21:22:14 -0400

    Flesh out generic runge kutta integration

--- sketchlib/RungeKuttaIntegrator.js
+++ sketchlib/RungeKuttaIntegrator.js
@@ -1,13 +1,11 @@
 /**
  * A set of static functions that perform vector addition/scalar multiplication
  * for some arbitrary type.
- * @typedef {Object} VectorSpace
  * @template V The vector type
+ * @typedef {Object} VectorSpace
  * @property {function(V, V): V} add
- * @property {function(V, number): V} scale
+ * @property {function(number, V): V} scale
  */
-
-import { ValueHistory } from "./ValueHistory";
 
 /**
  * A 4th order Runge-Kutta integrator. This offers greater precision than
@@ -16,20 +14,52 @@ import { ValueHistory } from "./ValueHistory";
  */
 export class RungeKuttaIntegrator {
   /**
-   * @param {VectorSpace} vector_space The underlying vector space over V
-   * @param {function(V): V} motion_equation The equation of motion that takes
-   * a state vector and returns the instananeous time derivative of each component
+   * @param {VectorSpace<V>} vector_space The underlying vector space over V
+   * @param {function(number, V): V} motion_equation The equation of motion that takes
+   * a state vector and current time and returns the instananeous time derivative of each variable
    * @param {V} initial_state The initial state at time 0
-   * @param {number} dt The time delta
    */
-  constructor(vector_space, motion_equation, initial_state, dt) {
+  constructor(vector_space, motion_equation, initial_state) {
     this.vector_space = vector_space;
     this.motion_equation = motion_equation;
     this.state = initial_state;
-    this.dt = dt;
+    this.time = 0;
   }
 
-  update(dt) {}
+  /**
+   * Update the state for the next integration step.
+   * @param {number} dt Time delta for the next simulation step
+   */
+  update(dt) {
+    const tangent1 = this.motion_equation(this.time, this.state);
+
+    const { add: add, scale: scale } = this.vector_space;
+
+    const half_dt = 0.5 * dt;
+    const projected_state1 = add(this.state, scale(half_dt, tangent1));
+    const tangent2 = this.motion_equation(
+      this.time + 0.5 * dt,
+      projected_state1
+    );
+
+    const projected_state2 = add(this.state, scale(half_dt, tangent2));
+    const tangent3 = this.motion_equation(
+      this.time + 0.5 * dt,
+      projected_state2
+    );
+
+    const projected_state3 = add(this.state, scale(dt, tangent3));
+    const tangent4 = this.motion_equation(this.time + dt, projected_state3);
+
+    // dt/6 * (t1 + 2 t2 + 2 t3 + t4)
+    const doubled_terms = scale(2, add(tangent2, tangent3));
+    const other_terms = add(tangent1, tangent4);
+    const in_parentheses = add(doubled_terms, other_terms);
+    const weighted_tangent = scale(dt / 6, in_parentheses);
+
+    this.state = add(this.state, weighted_tangent);
+    this.time += dt;
+  }
 }
 
 /*
