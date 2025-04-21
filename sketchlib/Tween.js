c3a1ed3bf0f2ddef93c25bc45f4d84151b5c5e41
Peter Gagliardi <ptrgags@gmail.com>
2025-04-20 20:22:31 -0400

    Sketch out Oklch as a class

--- sketchlib/Tween.js
+++ sketchlib/Tween.js
@@ -1,16 +1,6 @@
 import { Point } from "../pga2d/objects.js";
 import { clamp } from "./clamp.js";
-
-/**
- * Lerp for numbers
- * @param {number} a The first number
- * @param {number} b The second number
- * @param {number} t The time value
- * @returns {number} The blend of a and b proportional to t
- */
-function lerp(a, b, t) {
-  return (1 - t) * a + t * b;
-}
+import { lerp } from "./lerp.js";
 
 /**
  * An object to manage inbetweening for animation
