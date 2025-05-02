a52db6fcf7b29a40c4a6f0c2d42eb9b8d7b25f31
Peter Gagliardi <ptrgags@gmail.com>
2025-05-02 10:28:33 -0400

    Define more musical types

--- lab/lablib/tone_helpers/compile_music.js
+++ lab/lablib/tone_helpers/compile_music.js
@@ -1,4 +1,4 @@
-import { Melody, Rest } from "../music/Score.js";
+import { Melody, Note, Rest } from "../music/Score.js";
 import { Rational } from "../Rational.js";
 import { to_tone_time } from "./measure_notation.js";
 import { to_tone_pitch } from "./to_tone_pitch.js";
@@ -13,23 +13,28 @@ import { to_tone_pitch } from "./to_tone_pitch.js";
 export function compile_part_events(midi_melody) {
   const events = [];
   let offset = Rational.ZERO;
-  for (const note of midi_melody.values) {
+  for (const note of midi_melody.children) {
     if (note instanceof Rest) {
       offset = offset.add(note.duration);
       continue;
     }
 
-    const start = to_tone_time(offset);
-    const pitch = to_tone_pitch(note.pitch);
-    const dur = to_tone_time(note.duration);
+    if (note instanceof Note) {
+      const start = to_tone_time(offset);
+      const pitch = to_tone_pitch(note.pitch);
+      const dur = to_tone_time(note.duration);
 
-    /**
-     * @type {[string, [string, string]]}
-     */
-    const event = [start, [pitch, dur]];
+      /**
+       * @type {[string, [string, string]]}
+       */
+      const event = [start, [pitch, dur]];
 
-    events.push(event);
-    offset = offset.add(note.duration);
+      events.push(event);
+      offset = offset.add(note.duration);
+      continue;
+    }
+
+    throw new Error("other musical types not implemented");
   }
   return events;
 }
