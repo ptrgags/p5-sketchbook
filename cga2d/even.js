6d05d11f6f1ddd9bbb5ee9a21efeabe0f6a30687
Peter Gagliardi <ptrgags@gmail.com>
2025-07-11 08:44:26 -0400

    Sketch out start of CGA vectors

---
+++ cga2d/even.js
@@ -0,0 +1,23 @@
+export class CGAEven {
+  /**
+   * Constructor
+   * @param {number} s
+   * @param {number} xy
+   * @param {number} xp
+   * @param {number} xn
+   * @param {number} yp
+   * @param {number} yn
+   * @param {number} pn
+   * @param {number} xypn
+   */
+  constructor(s, xy, xp, xn, yp, yn, pn, xypn) {
+    this.s = s;
+    this.xy = xy;
+    this.xp = xp;
+    this.xn = xn;
+    this.yp = yp;
+    this.yn = yn;
+    this.pn = pn;
+    this.xypn = xypn;
+  }
+}
---
+++ cga2d/odd.js
@@ -0,0 +1,23 @@
+export class CGAOdd {
+  /**
+   * Constructor
+   * @param {number} x
+   * @param {number} y
+   * @param {number} p
+   * @param {number} n
+   * @param {number} xyp
+   * @param {number} xyn
+   * @param {number} xpn
+   * @param {number} ypn
+   */
+  constructor(x, y, p, n, xyp, xyn, xpn, ypn) {
+    this.x = x;
+    this.y = y;
+    this.p = p;
+    this.n = n;
+    this.xyp = xyp;
+    this.xyn = xyn;
+    this.xpn = xpn;
+    this.ypn = ypn;
+  }
+}
