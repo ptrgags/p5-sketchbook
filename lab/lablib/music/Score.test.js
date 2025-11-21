eee7f0cf30fdff7609e18ccb1f603ed0dd786285
Peter Gagliardi <ptrgags@gmail.com>
2025-11-21 22:43:27 +0000

    Add remaining tests for parse_cycle

--- lab/lablib/music/Score.test.js
+++ lab/lablib/music/Score.test.js
@@ -7,9 +7,9 @@ import {
   parse_melody,
   Rest,
 } from "./Score.js";
-import { N1, N2, N4, N4T, N8 } from "./durations.js";
+import { N1, N2, N4, N4T, N8, N8T } from "./durations.js";
 import { Rational } from "../Rational.js";
-import { C, C4, E, E4, G4, REST } from "./pitches.js";
+import { A4, AS4, B4, C, C4, E, E4, F4, G4, REST } from "./pitches.js";
 import { MidiPitch } from "./pitch_conversions.js";
 
 describe("Melody", () => {
@@ -99,7 +99,31 @@ describe("parse_cycle", () => {
     expect(result).toEqual(expected);
   });
 
-  it("parses 4-beat cycle", () => {});
+  it("parses 4-beat cycle", () => {
+    const cycle = [C4, E4, G4, B4];
 
-  it("parses nested cycle", () => {});
+    const result = parse_cycle(N1, cycle);
+
+    const expected = new Melody(
+      new Note(C4, N4),
+      new Note(E4, N4),
+      new Note(G4, N4),
+      new Note(B4, N4)
+    );
+    expect(result).toEqual(expected);
+  });
+
+  it("parses nested cycle", () => {
+    const cycle = [C4, [E4, F4], G4, [A4, AS4, B4]];
+
+    const result = parse_cycle(N1, cycle);
+
+    const expected = new Melody(
+      new Note(C4, N4),
+      new Melody(new Note(E4, N8), new Note(F4, N8)),
+      new Note(G4, N4),
+      new Melody(new Note(A4, N8T), new Note(AS4, N8T), new Note(B4, N8T))
+    );
+    expect(result).toEqual(expected);
+  });
 });
