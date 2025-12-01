fbfe2d739e1398703b60becb5a5550ad2f1c3c45
Peter Gagliardi <ptrgags@gmail.com>
2025-12-01 21:28:58 +0000

    WIP: Prototype score creation

--- lab/MusicalTree/MusicalTree.js
+++ lab/MusicalTree/MusicalTree.js
@@ -1,23 +1,19 @@
+import { Point } from "../../pga2d/objects.js";
 import { WIDTH, HEIGHT } from "../../sketchlib/dimensions.js";
 import { LSystem } from "../../sketchlib/LSystem.js";
 import { draw_primitive } from "../../sketchlib/p5_helpers/draw_primitive.js";
 import { CanvasMouseHandler } from "../lablib/CanvasMouseHandler.js";
 import { MouseInput } from "../lablib/MouseInput.js";
+import { N32 } from "../lablib/music/durations.js";
+import { B4, C4, E4, E6, F6, G6 } from "../lablib/music/pitches.js";
+import { Melody, Note, Rest, Score } from "../lablib/music/Score.js";
 import { MuteButton } from "../lablib/MuteButton.js";
 import { PlayButtonScene } from "../lablib/PlayButtonScene.js";
+import { Rational } from "../lablib/Rational.js";
 import { SoundManager } from "../lablib/SoundManager.js";
 
-const MOUSE = new CanvasMouseHandler();
-
-// Add scores here
-/**@type {import("../lablib/SoundManager.js").SoundManifest} */
-const SOUND_MANIFEST = {};
-
-//@ts-ignore
-const SOUND = new SoundManager(Tone, SOUND_MANIFEST);
-
 const TREE_LSYSTEM = new LSystem("Fa", {
-  a: "[+Fa]-Fa",
+  a: "[+Fa][-Fa]",
 });
 
 function count_symbols(str) {
@@ -33,12 +29,101 @@ function count_symbols(str) {
   return counts;
 }
 
-const strings = TREE_LSYSTEM.iterate(8);
-for (const str of strings) {
+const TREE_STRS = TREE_LSYSTEM.iterate(8);
+for (const str of TREE_STRS) {
   const counts = count_symbols(str);
   const ignore_a_len = str.length - counts.get("a");
   console.log(ignore_a_len, counts, str);
 }
+
+/*
+function render_tree(tree_str, max_depth) {
+  const DELTA_ANGLE = Math.PI / 3;
+  let position = Point.point(WIDTH / 2, (3 * HEIGHT) / 2);
+  let orientation = 0;
+  const stack = [];
+  let depth = 0;
+
+  for (const c of tree_str) {
+    if (c === "F") {
+    } else if (c === "[") {
+      depth++;
+      this.stack.push([position, orientation]);
+    } else if (c === "]") {
+      [position, orientation] = this.stack.pop();
+    }
+  }
+}*/
+
+function make_score(tree_str, max_depth) {
+  const draw_notes = [];
+  const stack_notes = [];
+  const turn_notes = [];
+
+  // Duration of the shortest note
+  const DUR = N32;
+  // For stack push/pops, it'll be hi-lo for push, lo-hi for pop
+  const STACK_HI = new Note(B4, DUR);
+  const STACK_LO = new Note(E4, DUR);
+
+  // For left/right turns, just play a single note, different for each direction
+  const TURN_LEFT = new Note(G6, DUR);
+  const TURN_RIGHT = new Note(F6, DUR);
+
+  let depth = 0;
+  for (const c of tree_str) {
+    if (c === "F") {
+      const note_length = max_depth - depth + 1;
+      const dur = DUR.mul(new Rational(note_length));
+      draw_notes.push(new Note(C4, dur));
+      stack_notes.push(new Rest(dur));
+      turn_notes.push(new Rest(dur));
+    } else if (c === "[") {
+      depth++;
+      const dur = DUR.mul(new Rational(2));
+      draw_notes.push(new Rest(dur));
+      stack_notes.push(new Melody(STACK_HI, STACK_LO));
+      turn_notes.push(new Rest(dur));
+    } else if (c === "]") {
+      depth--;
+      const dur = DUR.mul(new Rational(2));
+      draw_notes.push(new Rest(dur));
+      stack_notes.push(new Melody(new Note(E4, dur), new Note(B4, dur)));
+      turn_notes.push(new Rest(dur));
+    } else if (c === "+") {
+      draw_notes.push(new Rest(DUR));
+      stack_notes.push(new Rest(DUR));
+      turn_notes.push(TURN_LEFT);
+    } else if (c === "-") {
+      draw_notes.push(new Rest(DUR));
+      stack_notes.push(new Rest(DUR));
+      turn_notes.push(TURN_RIGHT);
+    } else {
+      continue;
+    }
+  }
+
+  return new Score({
+    parts: [
+      ["sine", new Melody(...draw_notes)],
+      ["square", new Melody(...stack_notes)],
+      ["poly", new Melody(...turn_notes)],
+    ],
+  });
+}
+
+const MOUSE = new CanvasMouseHandler();
+
+// Add scores here
+/**@type {import("../lablib/SoundManager.js").SoundManifest} */
+const SOUND_MANIFEST = {
+  scores: {
+    tree: make_score(TREE_STRS[3], 3),
+  },
+};
+
+//@ts-ignore
+const SOUND = new SoundManager(Tone, SOUND_MANIFEST);
 
 class SoundScene {
   /**
