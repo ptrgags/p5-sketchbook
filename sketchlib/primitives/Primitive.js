cacf5e0d0e2f8c6530f5fb741e5d49a125f92fb9
Peter Gagliardi <ptrgags@gmail.com>
2025-12-19 19:29:14 -0500

    Document intent of Primitive

--- sketchlib/primitives/Primitive.js
+++ sketchlib/primitives/Primitive.js
@@ -1,4 +1,16 @@
 /**
+ * Interface for geometric primitives that can be drawn when passed a p5
+ * instance. Some properties of primitives:
+ *
+ * - Primitives are composable. GroupPrimitive acts as a container
+ * for other primitives, with additional styling and transformations.
+ * - Often, I like to make primitives immutable. Construct as much as I can
+ * once at the start of a sketch, then render as often as needed.
+ * - However, in scenes with dynamic geometry, there may be cases where I
+ * make primitives with inner immutability if that helps with performance.
+ * - This decouples my sketch code (which uses a lot of custom types like Point)
+ * from the p5 drawing commands
+ *
  * @interface Primitive
  */
 export class Primitive {
