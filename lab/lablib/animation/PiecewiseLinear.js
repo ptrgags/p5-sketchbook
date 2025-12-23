99bcd3711cb47460352ae518ff01442d67614422
Peter Gagliardi <ptrgags@gmail.com>
2025-12-23 16:06:43 -0500

    Add some animation-related classes from split-sound

---
+++ lab/lablib/animation/PiecewiseLinear.js
@@ -0,0 +1,42 @@
+import { lerp } from "../../../sketchlib/lerp.js";
+
+/**
+ * Piecewise Linear function defined by a number of points listed in
+ * order by x coordinate. In between the x values, values are interpolated
+ * linearly.
+ */
+export class PiecewiseLinear {
+  /**
+   * Constructor
+   * @param {[number, number][]} points List of (x, y) pairs. The x values must be in sorted order!
+   */
+  constructor(points) {
+    if (points.length < 1) {
+      throw new Error("points must have at least one entry!");
+    }
+
+    this.points = points;
+  }
+
+  /**
+   * Get the value of the function
+   * @param {number} x input value
+   * @returns {number} The output value of the function
+   */
+  value(x) {
+    if (x < this.points[0][0]) {
+      return this.points[0][1];
+    }
+
+    if (x > this.points[this.points.length - 1][0]) {
+      return this.points[this.points.length - 1][1];
+    }
+
+    const larger_index = this.points.findIndex((point) => point[0] > x);
+    const smaller_index = larger_index - 1;
+
+    const [large_x, large_y] = this.points[larger_index];
+    const [small_x, small_y] = this.points[smaller_index];
+    return lerp(small_y, large_y, (x - small_x) / (large_x - small_x));
+  }
+}
---
+++ lab/lablib/animation/PiecewiseLinear.test.js
@@ -0,0 +1,67 @@
+import { describe, it, expect } from "vitest";
+import { PiecewiseLinear } from "./PiecewiseLinear";
+
+describe("PiecewiseLinear", () => {
+  it("with no points throws error", () => {
+    expect(() => {
+      return new PiecewiseLinear([]);
+    }).toThrowError("points must have at least one entry!");
+  });
+
+  it("with one point returns constant", () => {
+    const curve = new PiecewiseLinear([[4, 3]]);
+
+    const result = curve.value(2.0);
+
+    expect(result).toBe(3);
+  });
+
+  it("with value smaller than first x clamps", () => {
+    const curve = new PiecewiseLinear([
+      [2.5, 0.25],
+      [3.0, 0.5],
+      [5.0, 0.75],
+    ]);
+
+    const result = curve.value(1.0);
+
+    expect(result).toBe(0.25);
+  });
+
+  it("with value larger than last x clamps", () => {
+    const curve = new PiecewiseLinear([
+      [2.5, 0.25],
+      [3.0, 0.5],
+      [5.0, 0.75],
+    ]);
+
+    const result = curve.value(8.0);
+
+    expect(result).toBe(0.75);
+  });
+
+  it("with value at point returns correct value", () => {
+    const curve = new PiecewiseLinear([
+      [2.5, 0.25],
+      [3.0, 0.5],
+      [5.0, 0.75],
+    ]);
+
+    const result = curve.value(3.0);
+
+    expect(result).toBeCloseTo(0.5);
+  });
+
+  it("with value in between returns correct value", () => {
+    const curve = new PiecewiseLinear([
+      [2.5, 0.25],
+      [3.0, 0.5],
+      [5.0, 0.75],
+    ]);
+
+    const result = curve.value(4.5);
+
+    // should be lerp(0.5, 0.75, (4.5-3)/(5-3)) = 0.6875
+    expect(result).toBeCloseTo(0.6875);
+  });
+});
