ccf7badf0149e66b4d0543359c56fce58b659806
Peter Gagliardi <ptrgags@gmail.com>
2025-12-29 16:56:44 -0500

    Add just score_to_midi code

---
+++ lab/lablib/midi/MidiFile.js
@@ -0,0 +1,206 @@
+/**
+ * @enum {number}
+ */
+export const MIDIFormat = {
+  SINGLE_TRACK: 0,
+  MULTI_PARALLEL: 1,
+  MULTI_SEQUENCE: 2,
+};
+Object.freeze(MIDIFormat);
+
+export class MIDIHeader {
+  /**
+   * Constructor
+   * @param {MIDIFormat} format MIDI format
+   * @param {number} num_tracks Number of tracks in the file
+   * @param {number} ticks_per_quarter How many ticks per quarter note
+   */
+  constructor(format, num_tracks, ticks_per_quarter) {
+    this.format = format;
+    this.num_tracks = num_tracks;
+    this.ticks_per_quarter = ticks_per_quarter;
+  }
+}
+
+/**
+ * @enum {number}
+ */
+export const MIDIMessageType = {
+  NOTE_OFF: 0b1000,
+  NOTE_ON: 0b1001,
+  POLY_AFTERTOUCH: 0b1010,
+  CONTROL_CHANGE: 0b1011,
+  PROGRAM_CHANGE: 0b1100,
+  CHANNEL_AFTERTOUCH: 0b1101,
+  PITCH_BEND: 0b1110,
+  CHANNEL_MODE: 0b1011,
+  SYSTEM: 0b1111,
+};
+Object.freeze(MIDIMessageType);
+
+/**
+ * Get the length of a MIDI message (not including meta or sysex events)
+ * @param {number} message_type The MIDI message type
+ * @returns {0 | 1 | 2} The number of data bytes for this message type.
+ */
+export function get_data_length(message_type) {
+  switch (message_type) {
+    case MIDIMessageType.NOTE_OFF:
+    case MIDIMessageType.NOTE_ON:
+    case MIDIMessageType.POLY_AFTERTOUCH:
+    case MIDIMessageType.CONTROL_CHANGE:
+    case MIDIMessageType.PITCH_BEND:
+    case MIDIMessageType.CHANNEL_MODE:
+      return 2;
+    case MIDIMessageType.PROGRAM_CHANGE:
+    case MIDIMessageType.CHANNEL_AFTERTOUCH:
+      return 1;
+    default:
+      return 0;
+  }
+}
+
+export class MIDIMessage {
+  /**
+   * Constructor
+   * @param {number} tick_delta time since the previous message in ticks
+   * @param {number} message_type the MIDI Message type
+   * @param {number} channel The channel number 0-15
+   * @param {Uint8Array} data 1-3 data bytes as a u8 array
+   */
+  constructor(tick_delta, message_type, channel, data) {
+    this.tick_delta = tick_delta;
+    this.message_type = message_type;
+    this.channel = channel;
+    this.data = data;
+  }
+
+  /**
+   * Shorthand for a note on event
+   * @param {number} tick_delta Time delta for message
+   * @param {number} channel Channel number 0-15
+   * @param {number} pitch MIDI note number
+   * @param {number} velocity MIDI velocity 0-127
+   * @returns {MIDIMessage} The note event
+   */
+  static note_on(tick_delta, channel, pitch, velocity) {
+    return new MIDIMessage(
+      tick_delta,
+      MIDIMessageType.NOTE_ON,
+      channel,
+      new Uint8Array([pitch, velocity])
+    );
+  }
+
+  /**
+   * Shorthand for a note off event
+   * @param {number} tick_delta Time delta for message
+   * @param {number} channel Channel number 0-15
+   * @param {number} pitch MIDI note number
+   * @param {number} [velocity=0] MIDI velocity 0-127
+   * @returns {MIDIMessage} The note event
+   */
+  static note_off(tick_delta, channel, pitch, velocity = 0) {
+    return new MIDIMessage(
+      tick_delta,
+      MIDIMessageType.NOTE_OFF,
+      channel,
+      new Uint8Array([pitch, velocity])
+    );
+  }
+}
+
+/**
+ * @enum {number}
+ */
+export const MIDIMetaType = {
+  SEQUENCE_NUMBER: 0x00,
+  TEXT: 0x01,
+  COPYRIGHT: 0x02,
+  TRACK_NAME: 0x03,
+  LYRIC: 0x05,
+  MARKER: 0x06,
+  CUE_POINT: 0x07,
+  CHANNEL_PREFIX: 0x20,
+  END_OF_TRACK: 0x2f,
+  SET_TEMPO: 0x51,
+  SMPTE_OFFSET: 0x54,
+  TIME_SIGNATURE: 0x58,
+  KEY_SIGNATURE: 0x59,
+  SEQUENCER_SPECIFIC: 0x7f,
+};
+Object.freeze(MIDIMetaType);
+
+/**
+ * Type for all other Meta events that I don't support
+ */
+export class MIDIMetaEvent {
+  /**
+   * Constructor
+   * @param {number} tick_delta Time delta in ticks
+   * @param {number} meta_type MIDIMetaType
+   * @param {Uint8Array} payload Raw bytes of message
+   */
+  constructor(tick_delta, meta_type, payload) {
+    this.tick_delta = tick_delta;
+    this.meta_type = meta_type;
+    this.payload = payload;
+  }
+
+  /**
+   * Shorthand for making an end of track message
+   * @param {number} tick_delta
+   * @returns
+   */
+  static end_of_track(tick_delta) {
+    return new MIDIMetaEvent(
+      tick_delta,
+      MIDIMetaType.END_OF_TRACK,
+      new Uint8Array(0)
+    );
+  }
+}
+
+export class MIDISysex {
+  /**
+   * Constructor
+   * @param {number} tick_delta Time delta in ticks
+   * @param {Uint8Array} data Payload of the sysex message
+   */
+  constructor(tick_delta, data) {
+    this.tick_delta = tick_delta;
+    this.data = data;
+  }
+}
+
+/**
+ * @typedef {MIDIMessage | MIDIMetaEvent | MIDISysex} MIDIEvent
+ */
+
+export class MIDITrack {
+  /**
+   * constructor
+   * @param {MIDIEvent[]} events
+   */
+  constructor(events) {
+    this.events = events;
+  }
+}
+
+export class MIDIFile {
+  /**
+   * Constructor
+   * @param {MIDIHeader} header The file header
+   * @param {MIDITrack[]} tracks One or more tracks
+   */
+  constructor(header, tracks) {
+    this.header = header;
+    this.tracks = tracks;
+
+    if (this.header.num_tracks !== this.tracks.length) {
+      throw new Error(
+        `incorrect number of tracks, expected ${this.header.num_tracks}, got ${this.tracks.length}`
+      );
+    }
+  }
+}
---
+++ lab/lablib/midi/score_to_midi.js
@@ -0,0 +1,93 @@
+import { Score } from "../music/Score.js";
+import { to_events } from "../music/Timeline.js";
+import { Rational } from "../Rational.js";
+import {
+  MIDIFile,
+  MIDIFormat,
+  MIDIHeader,
+  MIDIMessage,
+  MIDIMessageType,
+  MIDIMetaEvent,
+  MIDIMetaType,
+  MIDITrack,
+} from "./MidiFile.js";
+
+// Ableton's export format
+const TICKS_PER_QUARTER = 96;
+// Assuming 4/4 time
+const TICKS_PER_MEASURE = TICKS_PER_QUARTER / 4;
+const DEFAULT_VELOCITY = 127;
+
+/**
+ * Get the number of ticks
+ * @param {Rational} time_measures How many measures since the start of the score as a fraction
+ * @returns {number} integer number of MIDI ticks
+ */
+function to_ticks(time_measures) {
+  return Math.round(time_measures.real * TICKS_PER_MEASURE);
+}
+
+/**
+ * Convert a Score to a file of MIDI messages
+ * @param {Score<number>} score Score in terms of midi notes
+ * @return {MIDIFile} The converted MIDI file
+ */
+export function score_to_midi(score) {
+  const header = new MIDIHeader(MIDIFormat.SINGLE_TRACK, 1, TICKS_PER_QUARTER);
+
+  /**
+   * @type {[number, number, MIDIMessageType, number][]}
+   */
+  const all_events = [];
+  for (const [i, [, music]] of score.parts.entries()) {
+    const part_events = to_events(Rational.ZERO, music).flatMap(
+      ([note, start, end]) => {
+        /**
+         * @type {[number, number, MIDIMessageType, number]}
+         */
+        const start_event = [
+          to_ticks(start),
+          i,
+          MIDIMessageType.NOTE_ON,
+          note.pitch,
+        ];
+        /**
+         * @type {[number, number, MIDIMessageType, number]}
+         */
+        const end_event = [
+          to_ticks(end),
+          i,
+          MIDIMessageType.NOTE_OFF,
+          note.pitch,
+        ];
+        return [start_event, end_event];
+      }
+    );
+    all_events.push(...part_events);
+  }
+
+  const sorted = all_events.sort((a, b) => a[0] - b[0]);
+
+  const events = [];
+  let prev_tick = 0;
+  for (const [absolute_tick, channel, message_type, pitch] of sorted) {
+    const delta = absolute_tick - prev_tick;
+    const event = new MIDIMessage(
+      delta,
+      message_type,
+      channel,
+      new Uint8Array([pitch, DEFAULT_VELOCITY])
+    );
+    events.push(event);
+
+    prev_tick = absolute_tick;
+  }
+
+  // the End of Track message is required
+  events.push(
+    new MIDIMetaEvent(0, MIDIMetaType.END_OF_TRACK, new Uint8Array(0))
+  );
+
+  const track = new MIDITrack(events);
+  return new MIDIFile(header, [track]);
+}
