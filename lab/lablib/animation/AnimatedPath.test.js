8f01734241887e53dcac59c08999fdb8ac5bad1a
Peter Gagliardi <ptrgags@gmail.com>
2026-01-01 13:35:34 -0500

    Flesh out missing tests

--- lab/lablib/animation/AnimatedPath.test.js
+++ lab/lablib/animation/AnimatedPath.test.js
@@ -4,6 +4,10 @@ import { LinePrimitive } from "../../../sketchlib/primitives/LinePrimitive.js";
 import { Point } from "../../../pga2d/Point.js";
 import { GroupPrimitive } from "../../../sketchlib/primitives/GroupPrimitive.js";
 import { group } from "../../../sketchlib/primitives/shorthand.js";
+import { PGA_MATCHERS } from "../../../pga2d/pga_matchers.js";
+import { Direction } from "../../../pga2d/Direction.js";
+
+expect.extend(PGA_MATCHERS);
 
 const A = new Point(0, 0);
 const B = new Point(1, 0);
@@ -21,7 +25,39 @@ function make_path() {
 }
 
 describe("AnimatedPath", () => {
-  describe("is_done", () => {});
+  describe("is_done", () => {
+    it("with time before start returns false", () => {
+      const path = make_path();
+
+      const result = path.is_done(0);
+
+      expect(result).toBe(false);
+    });
+
+    it("with time within range returns false", () => {
+      const path = make_path();
+
+      const result = path.is_done(2);
+
+      expect(result).toBe(false);
+    });
+
+    it("with time exactly at end returns false", () => {
+      const path = make_path();
+
+      const result = path.is_done(5);
+
+      expect(result).toBe(false);
+    });
+
+    it("with time after end returns true", () => {
+      const path = make_path();
+
+      const result = path.is_done(7);
+
+      expect(result).toBe(true);
+    });
+  });
 
   describe("get_position", () => {
     it("with empty path returns undefined", () => {
@@ -31,11 +67,97 @@ describe("AnimatedPath", () => {
 
       expect(result).toBeUndefined();
     });
+
+    it("with time before start returns start point", () => {
+      const path = make_path();
+
+      const result = path.get_position(1);
+
+      expect(result).toBePoint(A);
+    });
+
+    it("with time in middle of range returns correct position", () => {
+      const path = make_path();
+
+      const result = path.get_position(3.25);
+
+      const expected = new Point(0.75, 1);
+      expect(result).toBePoint(expected);
+    });
+
+    it("with time after end returns end point", () => {
+      const path = make_path();
+
+      const result = path.get_position(5);
+
+      expect(result).toBePoint(A);
+    });
   });
 
-  describe("get_tangent", () => {});
+  describe("get_tangent", () => {
+    it("with empty path returns undefined", () => {
+      const path = new AnimatedPath([], 0, 1);
 
-  describe("render", () => {});
+      const result = path.get_tangent(0.5);
+
+      expect(result).toBeUndefined();
+    });
+
+    it("with time before start returns tangent at start", () => {
+      const path = make_path();
+
+      const result = path.get_tangent(1);
+
+      expect(result).toBeDirection(Direction.DIR_X);
+    });
+
+    it("with time in middle of range returns correct position", () => {
+      const path = make_path();
+
+      const result = path.get_tangent(3.25);
+
+      const expected = Direction.DIR_X.neg();
+      expect(result).toBeDirection(expected);
+    });
+
+    it("with time after end returns tangent at end of path", () => {
+      const path = make_path();
+
+      const result = path.get_tangent(5);
+
+      const expected = Direction.DIR_Y.neg();
+      expect(result).toBeDirection(expected);
+    });
+  });
+
+  describe("render", () => {
+    it("with time less than start returns empty group", () => {
+      const path = make_path();
+
+      const result = path.render(-1);
+
+      expect(result).toEqual(GroupPrimitive.EMPTY);
+    });
+
+    it("with time in between start and end returns partial path", () => {
+      const path = make_path();
+
+      const result = path.render(2.5);
+
+      const half_bc = new LinePrimitive(B, new Point(1, 0.5));
+      const expected = group(AB, half_bc);
+      expect(result).toBe(expected);
+    });
+
+    it("with time greater than end returns whole path", () => {
+      const path = make_path();
+
+      const result = path.render(8);
+
+      const expected = group(AB, BC, CD, DA);
+      expect(result).toEqual(expected);
+    });
+  });
 
   describe("render_between", () => {
     it("with time_a > time_b returns empty group", () => {
@@ -77,7 +199,7 @@ describe("AnimatedPath", () => {
       const result = path.render_between(2, 10);
 
       const expected = group(BC, CD, DA);
-      expect(result).toBe(GroupPrimitive.EMPTY);
+      expect(result).toBe(expected);
     });
 
     it("with both times after end returns empty path", () => {
