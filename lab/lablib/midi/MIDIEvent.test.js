961e9c81da97609e35fd50108a04db3495455a3f
Peter Gagliardi <ptrgags@gmail.com>
2026-01-01 18:13:37 -0500

    Add unit tests for MIDIEvent decode functions

--- lab/lablib/midi/MIDIEvent.test.js
+++ lab/lablib/midi/MIDIEvent.test.js
@@ -5,7 +5,7 @@ import {
   MIDIMetaType,
   MIDISysex,
 } from "./MIDIEvent.js";
-import { C4, G4 } from "../music/pitches.js";
+import { C4, G4, G5 } from "../music/pitches.js";
 
 function make_empty_view(length) {
   return new DataView(new ArrayBuffer(length));
@@ -16,6 +16,15 @@ function make_view(bytes) {
 }
 
 describe("MIDIMessage", () => {
+  describe("note_on", () => {
+    it("with velocity 0 returns a converted note off", () => {
+      const actually_off = MIDIMessage.note_on(2, C4, 0);
+
+      const expected = MIDIMessage.note_off(2, C4, 0);
+      expect(actually_off).toEqual(expected);
+    });
+  });
+
   describe("encode", () => {
     it("encodes note on event", () => {
       const note_on = MIDIMessage.note_on(1, C4, 64);
@@ -36,6 +45,41 @@ describe("MIDIMessage", () => {
 
       const expected = make_view([0x81, G4, 0]);
       expect(data_view).toEqual(expected);
+      expect(after).toBe(3);
+    });
+  });
+
+  describe("decode", () => {
+    it("decodes note on event", () => {
+      const status_byte = 0x92;
+      const data_view = make_view([status_byte, G5, 127]);
+
+      const [result, after] = MIDIMessage.decode(status_byte, data_view, 1);
+
+      const expected = MIDIMessage.note_on(2, G5, 127);
+      expect(result).toEqual(expected);
+      expect(after).toBe(3);
+    });
+
+    it("decodes note off event", () => {
+      const status_byte = 0x82;
+      const data_view = make_view([status_byte, G5, 127]);
+
+      const [result, after] = MIDIMessage.decode(status_byte, data_view, 1);
+
+      const expected = MIDIMessage.note_off(2, G5, 127);
+      expect(result).toEqual(expected);
+      expect(after).toBe(3);
+    });
+
+    it("decodes with offset", () => {
+      const status_byte = 0x82;
+      const data_view = make_view([0, 0, 0, 0, 0, status_byte, G5, 127]);
+
+      const [result, after] = MIDIMessage.decode(status_byte, data_view, 5 + 1);
+
+      const expected = MIDIMessage.note_off(2, G5, 127);
+      expect(result).toEqual(expected);
       expect(after).toBe(3);
     });
   });
@@ -63,6 +107,47 @@ describe("MIDIMetaEvent", () => {
       expect(after).toBe(4);
     });
   });
+
+  describe("decode", () => {
+    it("decodes meta event", () => {
+      const data_view = make_view([
+        MIDIMetaEvent.MAGIC,
+        MIDIMetaType.CHANNEL_PREFIX,
+        3,
+      ]);
+
+      const [result, after] = MIDIMetaEvent.decode(data_view, 1);
+
+      const expected = new MIDIMetaEvent(
+        MIDIMetaType.CHANNEL_PREFIX,
+        new Uint8Array([3])
+      );
+      expect(result).toEqual(expected);
+      expect(after).toBe(3);
+    });
+
+    it("decodes with offset", () => {
+      const data_view = make_view([
+        0,
+        0,
+        0,
+        0,
+        0,
+        0xff,
+        MIDIMetaType.CHANNEL_PREFIX,
+        3,
+      ]);
+
+      const [result, after] = MIDIMetaEvent.decode(data_view, 1 + 5);
+
+      const expected = new MIDIMetaEvent(
+        MIDIMetaType.CHANNEL_PREFIX,
+        new Uint8Array([3])
+      );
+      expect(result).toEqual(expected);
+      expect(after).toBe(3);
+    });
+  });
 });
 
 describe("MIDISysex", () => {
@@ -86,6 +171,47 @@ describe("MIDISysex", () => {
       ]);
       expect(data_view).toEqual(expected);
       expect(after).toBe(7);
+    });
+  });
+
+  describe("decode", () => {
+    it("decodes sysex event", () => {
+      const data_view = make_view([
+        MIDISysex.MAGIC,
+        3,
+        1,
+        2,
+        3,
+        MIDISysex.END_OF_SYSEX,
+      ]);
+
+      const [result, after] = MIDISysex.decode(data_view, 1);
+
+      const expected = new MIDISysex(new Uint8Array([1, 2, 3]));
+      expect(result).toEqual(expected);
+      expect(after).toBe(3);
+    });
+
+    it("decodes with offset", () => {
+      const data_view = make_view([
+        0,
+        0,
+        0,
+        0,
+        0,
+        MIDISysex.MAGIC,
+        4,
+        1,
+        2,
+        3,
+        MIDISysex.END_OF_SYSEX,
+      ]);
+
+      const [result, after] = MIDISysex.decode(data_view, 1 + 5);
+
+      const expected = new MIDISysex(new Uint8Array([1, 2, 3]));
+      expect(result).toEqual(expected);
+      expect(after).toBe(3);
     });
   });
 });
