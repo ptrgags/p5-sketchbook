04cad900be6843e385ab83814d0602fbd96c3dfe
Peter Gagliardi <ptrgags@gmail.com>
2026-01-07 16:59:56 +0000

    Make the door shapes

--- lab/AnimatedTangle/patterns/doors.js
+++ lab/AnimatedTangle/patterns/doors.js
@@ -1,7 +1,8 @@
 import { Direction } from "../../../pga2d/Direction.js";
 import { Point } from "../../../pga2d/Point.js";
 import { GroupPrimitive } from "../../../sketchlib/primitives/GroupPrimitive.js";
-import { group } from "../../../sketchlib/primitives/shorthand.js";
+import { PolygonPrimitive } from "../../../sketchlib/primitives/PolygonPrimitive.js";
+import { group, xform } from "../../../sketchlib/primitives/shorthand.js";
 import { Transform } from "../../../sketchlib/primitives/Transform.js";
 import { Style } from "../../../sketchlib/Style.js";
 import { PALETTE_CORAL, PALETTE_SKY, Values } from "../theme_colors.js";
@@ -51,9 +52,69 @@ const BARBER_POLE = group(
   })
 );
 
+const DOOR_HEIGHT = 150;
+const LOWER_DOOR = new PolygonPrimitive(
+  [
+    // bottom points on door
+    new Point(0, DOOR_HEIGHT),
+    new Point(50, DOOR_HEIGHT),
+    // square shapes that jut in and out
+    new Point(50, 0),
+    new Point(75 / 2, 0),
+    new Point(75 / 2, -25 / 2),
+    new Point(25, -25 / 2),
+    new Point(25, 25 / 2),
+    new Point(25 / 2, 25 / 2),
+    new Point(25 / 2, 0),
+    new Point(0, 0),
+  ],
+  true
+);
+const UPPER_DOOR = new PolygonPrimitive(
+  [
+    // top points on door
+    new Point(50, -DOOR_HEIGHT),
+    new Point(0, -DOOR_HEIGHT),
+    // Same square shapes as for the lower door, but wound the other way
+    new Point(0, 0),
+    new Point(25 / 2, 0),
+    new Point(25 / 2, 25 / 2),
+    new Point(25, 25 / 2),
+    new Point(25, -25 / 2),
+    new Point(75 / 2, -25 / 2),
+    new Point(75 / 2, 0),
+    new Point(50, 0),
+  ],
+  true
+);
+
+class Door {
+  constructor(shut_offset) {
+    this.shut_point = shut_offset;
+    this.lower_xform = new Transform(shut_offset);
+    this.upper_xform = new Transform(shut_offset);
+
+    this.primitive = group(
+      xform(LOWER_DOOR, this.lower_xform),
+      xform(UPPER_DOOR, this.upper_xform)
+    );
+  }
+
+  render() {
+    return this.primitive;
+  }
+}
+
 class Doors {
   constructor() {
-    this.primitive = BARBER_POLE;
+    this.doors = [
+      new Door(new Direction(0, 700)),
+      new Door(new Direction(75, 675)),
+      new Door(new Direction(150, 650)),
+      new Door(new Direction(225, 675)),
+    ];
+
+    this.primitive = group(BARBER_POLE, ...this.doors.map((x) => x.render()));
   }
 
   update(time) {
