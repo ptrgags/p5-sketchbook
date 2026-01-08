4362807dd913e8c6dce76ac82dafeec28231b2fa
Peter Gagliardi <ptrgags@gmail.com>
2026-01-08 21:57:13 +0000

    sketch out the rough shape of the polyp

--- lab/AnimatedTangle/patterns/polyps.js
+++ lab/AnimatedTangle/patterns/polyps.js
@@ -1,7 +1,20 @@
+import { Direction } from "../../../pga2d/Direction.js";
 import { Point } from "../../../pga2d/Point.js";
 import { CirclePrimitive } from "../../../sketchlib/primitives/CirclePrimitive.js";
 import { GroupPrimitive } from "../../../sketchlib/primitives/GroupPrimitive.js";
+import { LinePrimitive } from "../../../sketchlib/primitives/LinePrimitive.js";
 import { group } from "../../../sketchlib/primitives/shorthand.js";
+import { Tween } from "../../../sketchlib/Tween.js";
+
+const TENTACLE_MIN = 10;
+const TENTACLE_MAX = 30;
+const TENTACLE_CIRCLE_RADIUS = 3;
+const MOUTH_MIN = 15;
+const MOUTH_MAX = 20;
+const SIXTH_ROOTS = Direction.roots_of_unity(6);
+
+const EXTEND_TENTACLES = Tween.scalar(TENTACLE_MIN, TENTACLE_MAX, 0, 1);
+const OPEN_MOUTH = Tween.scalar(MOUTH_MIN, MOUTH_MAX, 0, 1);
 
 class Polyp {
   /**
@@ -10,7 +23,27 @@ class Polyp {
    */
   constructor(position) {
     this.position = position;
-    this.primitive = new CirclePrimitive(this.position, 25);
+
+    this.mouth_back = new CirclePrimitive(this.position, MOUTH_MAX);
+    this.mouth_front = new CirclePrimitive(this.position, MOUTH_MIN);
+
+    this.tentacle_lines = SIXTH_ROOTS.map((dir) => {
+      const anchor_point = this.position.add(dir.scale(MOUTH_MIN));
+      const extend_point = this.position.add(dir.scale(TENTACLE_MAX));
+      return new LinePrimitive(anchor_point, extend_point);
+    });
+
+    this.tentacle_circles = SIXTH_ROOTS.map((dir) => {
+      const position = this.position.add(dir.scale(TENTACLE_MAX));
+      return new CirclePrimitive(position, TENTACLE_CIRCLE_RADIUS);
+    });
+
+    this.primitive = group(
+      this.mouth_back,
+      ...this.tentacle_lines,
+      ...this.tentacle_circles,
+      this.mouth_front
+    );
   }
 
   update(time) {}
@@ -22,7 +55,7 @@ class Polyp {
 
 export class Polyps {
   constructor() {
-    this.polyps = [new Polyp(new Point(100, 500))];
+    this.polyps = [new Polyp(new Point(125, 500))];
 
     this.primitive = group(...this.polyps.map((x) => x.render()));
   }
