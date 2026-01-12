7450f9a92576af4ba175caad6e88f815d968e718
Peter Gagliardi <ptrgags@gmail.com>
2026-01-12 22:42:28 +0000

    Make a dither matrix

--- Ditherhythm/Ditherhythm.js
+++ Ditherhythm/Ditherhythm.js
@@ -1,16 +1,28 @@
 import { Direction } from "../pga2d/Direction.js";
 import { Point } from "../pga2d/Point.js";
 import { WIDTH, HEIGHT } from "../sketchlib/dimensions.js";
+import { Grid, Index2D } from "../sketchlib/Grid.js";
 import { RectPrimitive } from "../sketchlib/primitives/RectPrimitive.js";
 import { IsoGrid } from "./IsoGrid.js";
+
+// See https://en.wikipedia.org/wiki/Ordered_dithering#Threshold_map
+const BAYER8 = [
+  [0, 32, 8, 40, 2, 34, 10, 42],
+  [48, 16, 56, 24, 50, 18, 58, 26],
+  [12, 44, 4, 36, 14, 46, 6, 38],
+  [60, 28, 52, 20, 62, 30, 54, 22],
+  [3, 35, 11, 43, 1, 33, 9, 41],
+  [51, 19, 59, 27, 49, 17, 57, 25],
+  [15, 47, 7, 39, 13, 45, 5, 37],
+  [63, 31, 55, 23, 61, 29, 53, 21],
+];
 
 const ISO_GRID = new IsoGrid(
   new RectPrimitive(new Point(50, 50), new Direction(200, 200)),
   8,
   8,
   (row, col) => {
-    const index = row * 8 + col;
-    return (63 - index) / 63;
+    return BAYER8[row][col] / 63;
   }
 );
 
