b268be91edf8553e578d6f8e3cc92ee21d000385
Peter Gagliardi <ptrgags@gmail.com>
2026-01-31 16:07:21 -0500

    Update wavestack interface

--- sketchlib/instruments/Wavestack.js
+++ sketchlib/instruments/Wavestack.js
@@ -43,40 +43,54 @@ export class WaveStack {
       this.synth.volume.value = value;
     }
   }
+
   /**
-   *
-   * @param {import("tone")} tone
+   * Allocate Tone.js resources for this instrument
+   * @param {import('tone')} tone Tone module
+   * @param {import('tone').InputNode} destination Destination to connect to
+   * @param {number} voices Number of voices to allocate
    */
-  init_mono(tone) {
-    this.synth = new tone.Synth({
+  init(tone, destination, voices) {
+    const options = {
       oscillator: {
         type: this.waveform,
         count: this.count,
         spread: this.spread,
       },
       envelope: this.envelope,
-    }).toDestination();
+    };
+
+    if (voices > 1) {
+      this.synth = new tone.PolySynth({
+        options,
+        voice: tone.Synth,
+        maxPolyphony: 32,
+      }).toDestination();
+    } else {
+      this.synth = new tone.Synth(options);
+    }
+
+    this.synth.connect(destination);
   }
 
   /**
-   *
-   * @param {import("tone")} tone
+   * Play a note
+   * @param {string} pitch pitch in tone.js
+   * @param {string} duration Duration as a Tone.js duration
+   * @param {number} time Time as a Tone.js time
+   * @param {number} velocity Velocity as a number in [0.0, 1.0]
    */
-  init_poly(tone) {
-    this.synth = new tone.PolySynth(tone.Synth, {
-      oscillator: {
-        type: this.waveform,
-        count: this.count,
-        spread: this.spread,
-      },
-      envelope: this.envelope,
-    }).toDestination();
+  play_note(pitch, duration, time, velocity) {
+    if (!this.synth) {
+      throw new Error("play_note before initialization");
+    }
+
+    this.synth.triggerAttackRelease(pitch, duration, time, velocity);
   }
 
   destroy() {
     if (this.synth) {
       this.synth.dispose();
     }
-    throw new Error("Method not implemented.");
   }
 }
