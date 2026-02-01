d88f73ca35e62458bd9910bfa7304f2e48fa1ab6
Peter Gagliardi <ptrgags@gmail.com>
2026-02-01 22:44:55 +0000

    Add a couple tests

--- sketchlib/midi/midi_to_score.test.js
+++ sketchlib/midi/midi_to_score.test.js
@@ -3,10 +3,10 @@ import { MIDIFile, MIDIHeader } from "./MIDIFile.js";
 import { Part, Score } from "../music/Score.js";
 import { RelativeTimingTrack } from "./MIDITrack.js";
 import { midi_to_score } from "./midi_to_score.js";
-import { MIDIEvent, MIDIMessage } from "./MIDIEvent.js";
-import { C4 } from "../music/pitches.js";
-import { Note } from "../music/Music.js";
-import { N4 } from "../music/durations.js";
+import { MIDIEvent, MIDIMessage, MIDIMetaEvent } from "./MIDIEvent.js";
+import { C4, E4 } from "../music/pitches.js";
+import { Melody, Note, Rest } from "../music/Music.js";
+import { N1, N4 } from "../music/durations.js";
 
 const PPQ = MIDIHeader.DEFAULT_TICKS_PER_QUARTER;
 
@@ -47,6 +47,79 @@ describe("midi_to_score", () => {
         new Part("channel0", new Note(C4, N4), {
           midi_channel: 0,
           instrument_id: "channel0",
+        }),
+      ),
+      [],
+    ];
+    expect(result).toEqual(expected_score);
+  });
+
+  it("with tempo messages returns parsed tempos", () => {
+    // One held note, but getting faster lol
+    const with_tempo = make_midi(
+      [0, MIDIMetaEvent.set_tempo(128)],
+      [0, MIDIMessage.note_on(0, C4)],
+      [2 * PPQ, MIDIMetaEvent.set_tempo(256)],
+      [2 * PPQ, MIDIMessage.note_off(0, C4)],
+    );
+
+    const result = midi_to_score(with_tempo);
+
+    const expected_score = [
+      new Score(
+        new Part("channel0", new Note(C4, N1), { instrument_id: "channel0" }),
+      ),
+      [128, 256],
+    ];
+
+    expect(result).toEqual(expected_score);
+  });
+
+  it("with multiple channels returns multiple parts", () => {
+    const one_note = make_midi(
+      [0, MIDIMessage.note_on(0, C4)],
+      [0, MIDIMessage.note_on(1, E4)],
+      [PPQ, MIDIMessage.note_off(0, C4)],
+      [0, MIDIMessage.note_off(1, E4)],
+    );
+
+    const result = midi_to_score(one_note);
+
+    const expected_score = [
+      new Score(
+        new Part("channel0", new Note(C4, N4), {
+          midi_channel: 0,
+          instrument_id: "channel0",
+        }),
+        new Part("channel1", new Note(E4, N4), {
+          midi_channel: 1,
+          instrument_id: "channel1",
+        }),
+      ),
+      [],
+    ];
+    expect(result).toEqual(expected_score);
+  });
+
+  it("with channel that starts later returns correct timing", () => {
+    const one_note = make_midi(
+      [0, MIDIMessage.note_on(0, C4)],
+      [PPQ, MIDIMessage.note_off(0, C4)],
+      [0, MIDIMessage.note_on(1, E4)],
+      [PPQ, MIDIMessage.note_off(1, E4)],
+    );
+
+    const result = midi_to_score(one_note);
+
+    const expected_score = [
+      new Score(
+        new Part("channel0", new Note(C4, N4), {
+          midi_channel: 0,
+          instrument_id: "channel0",
+        }),
+        new Part("channel1", new Melody(new Rest(N4), new Note(E4, N4)), {
+          midi_channel: 1,
+          instrument_id: "channel1",
         }),
       ),
       [],
