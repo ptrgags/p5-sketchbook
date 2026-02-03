7c05d993b1e75846f0a40fb14dca24c1da4c237b
Peter Gagliardi <ptrgags@gmail.com>
2026-02-03 17:25:59 +0000

    Add a line to the sketch

--- CGATest/CGATest.js
+++ CGATest/CGATest.js
@@ -1,23 +1,21 @@
-import { Point } from "../pga2d/Point.js";
+import { Point } from "../sketchlib/pga2d/Point.js";
 import { Cline } from "../sketchlib/cga2d/Cline.js";
 import { Color } from "../sketchlib/Color.js";
 import { WIDTH, HEIGHT } from "../sketchlib/dimensions.js";
-import { CirclePrimitive } from "../sketchlib/primitives/CirclePrimitive.js";
+import { Line } from "../sketchlib/pga2d/Line.js";
+import { Circle } from "../sketchlib/primitives/Circle.js";
 import { group, style } from "../sketchlib/primitives/shorthand.js";
 import { Style } from "../sketchlib/Style.js";
 
-const CIRCLE = Cline.from_circle(new CirclePrimitive(new Point(250, 350), 50));
+const CIRCLE = Cline.from_circle(new Circle(new Point(250, 350), 50));
+const LINE = Cline.from_line(new Line(1, 2, 500));
 const POINT = Cline.from_point(new Point(100, 100));
 
 const LINE_STYLE = new Style({
-  fill: Color.RED,
-  stroke: Color.WHITE,
-});
-const POINT_STYLE = new Style({
-  fill: Color.BLUE,
+  stroke: Color.YELLOW,
 });
 
-const CGA_GEOM = group(style(CIRCLE, LINE_STYLE), style(POINT, POINT_STYLE));
+const CGA_GEOM = style([CIRCLE, POINT, LINE], LINE_STYLE);
 
 export const sketch = (p) => {
   p.setup = () => {
