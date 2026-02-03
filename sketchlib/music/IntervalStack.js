8b864e3b09c878654db65f5b7443d929fdbccc38
Peter Gagliardi <ptrgags@gmail.com>
2026-02-03 21:18:42 +0000

    Implement PitchClassStack and PitchStack

--- sketchlib/music/IntervalStack.js
+++ sketchlib/music/IntervalStack.js
@@ -24,18 +24,62 @@ export class IntervalStack {
    * Index -1 is P5 - P8
    * Index 3 is P1 + P8
    *
-   *
    * Note that if the interval stack spans more than an octave,
    * @param {number} index Index into the intervals list.
    */
   value(index) {
-    // The signed scale degree may be out of range, this just means
-    // up or down a number of octaves. For example, for a 7-note scale,
-    // -1 is the last note in the scale but down an octave (-1 = -1 * 7 + 6)
-    // 9 is the third note in the scale but up an octave (9 = 1 * 7 + 2)
     const octaves = Math.floor(index / this.intervals.length);
     const reduced_index = mod(index, this.intervals.length);
 
     return octaves * P8 + this.intervals[reduced_index];
+  }
+}
+
+export class PitchClassStack {
+  /**
+   * Constructor
+   * @param {IntervalStack} intervals
+   * @param {number} pitch_class Pitch class (e.g. C)
+   */
+  constructor(intervals, pitch_class) {
+    this.pitch_class = pitch_class;
+    this.intervals = intervals;
+  }
+
+  /**
+   * Get a ptich from the stack, starting from the root pitch and
+   * going up the stack one by one.
+   *
+   * When the index is out of range, the result is reduced mod 12
+   * @param {number} index Index of pitch class relative to the start
+   * @returns {number} An pitch class at the selected index
+   */
+  value(index) {
+    return mod(this.pitch_class + this.intervals.value(index), 12);
+  }
+}
+
+export class PitchStack {
+  /**
+   * Constructor
+   * @param {IntervalStack} intervals
+   * @param {number} root Root note as an absolute pitch (e.g. C4)
+   */
+  constructor(intervals, root) {
+    this.root = root;
+    this.intervals = intervals;
+  }
+
+  /**
+   * Get a ptich from the stack, starting from the root note and
+   * going up the stack one by one.
+   *
+   * When the index is out of range, the result will be transposed
+   * by the appropriate number of octaves
+   * @param {number} index Index of pitch relative to the start note
+   * @returns {number} An absolute pitch at the selected index
+   */
+  value(index) {
+    return this.root + this.intervals.value(index);
   }
 }
--- sketchlib/music/IntervalStack.test.js
+++ sketchlib/music/IntervalStack.test.js
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
-import { IntervalStack } from "./IntervalStack.js";
+import { IntervalStack, PitchClassStack, PitchStack } from "./IntervalStack.js";
 import { M3, P1, P5, P8 } from "./intervals.js";
+import { B, B4, B7, D, D3, G, G4 } from "./pitches.js";
 
 describe("IntervalStack", () => {
   it("value with index in range returns corresponding interval", () => {
@@ -26,6 +27,66 @@ describe("IntervalStack", () => {
     const result = stack.value(10);
 
     const expected = M3 + 3 * P8;
+    expect(result).toBe(expected);
+  });
+});
+
+describe("PitchClassStack", () => {
+  it("value with index in range returns corresponding interval", () => {
+    const major = new IntervalStack([P1, M3, P5]);
+    const g_major = new PitchClassStack(major, G);
+
+    const result = g_major.value(1);
+
+    expect(result).toBe(B);
+  });
+
+  it("value with negative value returns interval in a lower octave", () => {
+    const major = new IntervalStack([P1, M3, P5]);
+    const g_major = new PitchClassStack(major, G);
+
+    const result = g_major.value(-4);
+
+    expect(result).toBe(D);
+  });
+
+  it("value with value out of range returns interval at a higher octave", () => {
+    const major = new IntervalStack([P1, M3, P5]);
+    const g_major = new PitchClassStack(major, G);
+
+    const result = g_major.value(10);
+
+    expect(result).toBe(B);
+  });
+});
+
+describe("PitchStack", () => {
+  it("value with index in range returns corresponding interval", () => {
+    const major = new IntervalStack([P1, M3, P5]);
+    const g4_major = new PitchStack(major, G4);
+
+    const result = g4_major.value(1);
+
+    expect(result).toBe(B4);
+  });
+
+  it("value with negative value returns interval in a lower octave", () => {
+    const major = new IntervalStack([P1, M3, P5]);
+    const g4_major = new PitchStack(major, G4);
+
+    const result = g4_major.value(-4);
+
+    const expected = D3;
+    expect(result).toBe(expected);
+  });
+
+  it("value with value out of range returns interval at a higher octave", () => {
+    const major = new IntervalStack([P1, M3, P5]);
+    const g4_major = new PitchStack(major, G4);
+
+    const result = g4_major.value(10);
+
+    const expected = B7;
     expect(result).toBe(expected);
   });
 });
