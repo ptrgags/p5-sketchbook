3be40b915ff4c4a01b8d17bf2a0f474009fc2d96
Peter Gagliardi <ptrgags@gmail.com>
2026-02-18 08:28:11 -0500

    Add tests for MusicPatterns.melody

--- sketchlib/music/MusicPatterns.test.js
+++ sketchlib/music/MusicPatterns.test.js
@@ -4,7 +4,8 @@ import { B4, C4, E4, G4 } from "./pitches.js";
 import { N2, N4 } from "./durations.js";
 import { Velocity } from "./Velocity.js";
 import { MusicPatterns } from "./MusicPatterns.js";
-import { Note } from "./Music.js";
+import { make_note, Melody, Note, Rest } from "./Music.js";
+import { Rhythm } from "./Rhythm.js";
 
 describe("MusicPatterns", () => {
   describe("make_notes", () => {
@@ -75,6 +76,39 @@ describe("MusicPatterns", () => {
           new Note(B4, Velocity.F),
         ],
         N4,
+      );
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe("melody", () => {
+    it("with rhythm and pitch array makes melody", () => {
+      const rhythm = new Rhythm("x.x.", N4);
+      const pitches = [C4, E4];
+
+      const result = MusicPatterns.melody(rhythm, pitches);
+
+      const expected = new Melody(
+        make_note(C4, N4),
+        new Rest(N4),
+        make_note(E4, N4),
+        new Rest(N4),
+      );
+      expect(result).toEqual(expected);
+    });
+
+    it("with velocity array sets correct velocities", () => {
+      const rhythm = new Rhythm("x.x.", N4);
+      const pitches = [C4, E4];
+      const velocities = [Velocity.MP, Velocity.FF];
+
+      const result = MusicPatterns.melody(rhythm, pitches, velocities);
+
+      const expected = new Melody(
+        make_note(C4, N4, Velocity.MP),
+        new Rest(N4),
+        make_note(E4, N4, Velocity.FF),
+        new Rest(N4),
       );
       expect(result).toEqual(expected);
     });
