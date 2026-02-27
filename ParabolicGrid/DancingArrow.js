7bae5809c7ff98c227e60a8f30e638aa807d8c84
Peter Gagliardi <ptrgags@gmail.com>
2026-02-27 21:44:57 +0000

    Fluctuations, take 1

--- ParabolicGrid/DancingArrow.js
+++ ParabolicGrid/DancingArrow.js
@@ -1,25 +1,93 @@
 import { Animated } from "../sketchlib/animation/Animated.js";
 import { LoopCurve } from "../sketchlib/animation/LoopCurve.js";
-import { Hold, make_param } from "../sketchlib/animation/ParamCurve.js";
-import { N1 } from "../sketchlib/music/durations.js";
-import { Sequential } from "../sketchlib/music/Timeline.js";
+import {
+  Hold,
+  make_param,
+  ParamCurve,
+} from "../sketchlib/animation/ParamCurve.js";
+import { Ease } from "../sketchlib/Ease.js";
+import { N1, N16, N32, N4, N8 } from "../sketchlib/music/durations.js";
+import { Sequential, TimeInterval } from "../sketchlib/music/Timeline.js";
 import { Oklch } from "../sketchlib/Oklch.js";
 import { Direction } from "../sketchlib/pga2d/Direction.js";
 import { Point } from "../sketchlib/pga2d/Point.js";
 import { Circle } from "../sketchlib/primitives/Circle.js";
 import { group, style } from "../sketchlib/primitives/shorthand.js";
 import { VectorPrimitive } from "../sketchlib/primitives/VectorPrimitive.js";
+import { Rational } from "../sketchlib/Rational.js";
 import { Style } from "../sketchlib/Style.js";
+
+const ANGLE_MAX = Math.PI / 2;
+const ANGLE_3_4 = (3.0 * ANGLE_MAX) / 4.0;
+const ANGLE_HALF = ANGLE_MAX / 2;
+const ANGLE_1_4 = ANGLE_MAX / 4.0;
+const ANGLE_SMALL = Math.PI / 32;
+const EASE = Ease.in_out_cubic;
+
+/**
+ * Animate a fluctuation from a center value
+ * @param {number} center_value Value to hold at the start and end
+ * @param {number} anticipate_value Value at the peak of the anticipation phase (opposite the direction of desired motion)
+ * @param {number} followthrough_value Value at the peak of the followthrough phase (overshooting the center value)
+ * @param {Rational} anticipate_duration How long to to wind up before the drop
+ * @param {Rational} drop_duration How long the drop (the main forward motion... idk what to call that) is.
+ * @param {Rational} followthrough_duration How long the followthrough should be
+ * @returns {TimeInterval<ParamCurve>[]} [windup, drop, followthrough]. The curve will hit the center value at time anticipation_duration + 1/2 drop_duration
+ */
+function fluctuate(
+  center_value,
+  anticipate_value,
+  followthrough_value,
+  anticipate_duration,
+  drop_duration,
+  followthrough_duration,
+) {
+  const anticipate = make_param(
+    center_value,
+    anticipate_value,
+    anticipate_duration,
+    Ease.in_out_cubic,
+  );
+  const drop = make_param(
+    anticipate_value,
+    followthrough_value,
+    drop_duration,
+    Ease.in_out_cubic,
+  );
+  const followthrough = make_param(
+    followthrough_value,
+    center_value,
+    followthrough_duration,
+    Ease.in_out_cubic,
+  );
+  return [anticipate, drop, followthrough];
+}
 
 // animate back and forth between x (0 radians) and y (pi/2 radians)
 const CURVE_ANGLE = LoopCurve.from_timeline(
   new Sequential(
     // start of loop or transition from y to x
-    make_param(Math.PI / 2, 0, N1),
+    make_param(ANGLE_MAX, ANGLE_3_4, N4, EASE),
+    make_param(ANGLE_3_4, ANGLE_HALF, N4, EASE),
+    make_param(ANGLE_HALF, ANGLE_1_4, N4, EASE),
+    make_param(ANGLE_1_4, 0, N4, EASE),
     // x is animating, hold at 0
-    new Hold(N1),
+    //  |x...x...x...x...| 1/16
+    //
+    // Fluctuations happen
+    // ~|~..~~..~~..~~..~|~
+    new Hold(new Rational(3, 16)),
+    ...fluctuate(0, -ANGLE_SMALL, ANGLE_SMALL, N32, N16, N32),
+    new Hold(N8),
+    ...fluctuate(0, -2 * ANGLE_SMALL, 2 * ANGLE_SMALL, N32, N16, N32),
+    new Hold(N8),
+    ...fluctuate(0, -3 * ANGLE_SMALL, 3 * ANGLE_SMALL, N32, N16, N32),
+    new Hold(new Rational(3 / 16)),
     // transition from x to y
-    make_param(0, Math.PI / 2, N1),
+    make_param(0, ANGLE_1_4, N4, EASE),
+    make_param(ANGLE_1_4, ANGLE_HALF, N4, EASE),
+    make_param(ANGLE_HALF, ANGLE_3_4, N4, EASE),
+    make_param(ANGLE_3_4, ANGLE_MAX, N4, EASE),
     // y is animating, hold at pi/2
     new Hold(N1),
   ),
