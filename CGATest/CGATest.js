c01d3c2d23b47d1bf1c0ed23d895f182132005ca
Peter Gagliardi <ptrgags@gmail.com>
2026-02-28 21:56:23 -0500

    Add the attempted dorito to the scene

--- CGATest/CGATest.js
+++ CGATest/CGATest.js
@@ -20,16 +20,6 @@ import { ArcAngles } from "../sketchlib/ArcAngles.js";
 const CIRCLE = Cline.from_circle(new Circle(new Point(250, 350), 50));
 const LINE = Cline.from_line(new Line(3 / 5, 4 / 5, 350));
 const POINT = NullPoint.from_point(new Point(350, 250));
-
-// A line is the fixed point of a transformation
-const REFLECT = LINE.vector.normalize();
-const REFLECTED_POINT = POINT.transform(REFLECT);
-const REFLECTED_CIRCLE = CIRCLE.transform(REFLECT);
-
-const INVERT = CIRCLE.vector.normalize_o();
-const INVERTED_POINT = POINT.transform(INVERT);
-const INVERTED_LINE = LINE.transform(INVERT);
-
 const CHIP = [
   ClineArc.from_segment(
     new LinePrimitive(new Point(10, 10), new Point(10, 110)),
@@ -41,6 +31,17 @@ const CHIP = [
     new ArcPrimitive(new Point(10, 110), 100, new ArcAngles(0, Math.PI / 2)),
   ),
 ];
+
+// A line is the fixed point of a transformation
+const REFLECT = LINE.vector.normalize();
+const REFLECTED_POINT = POINT.transform(REFLECT);
+const REFLECTED_CIRCLE = CIRCLE.transform(REFLECT);
+const REFLECTED_CHIP = CHIP.map((x) => x.transform(REFLECT));
+
+const INVERT = CIRCLE.vector.normalize_o();
+const INVERTED_POINT = POINT.transform(INVERT);
+const INVERTED_LINE = LINE.transform(INVERT);
+const INVERTED_CHIP = CHIP.map((x) => x.transform(INVERT));
 
 const LINE_STYLE = new Style({
   stroke: Color.YELLOW,
@@ -55,12 +56,15 @@ const SPIN_STYLE = new Style({
   stroke: new Color(0, 255, 127),
 });
 
-const ORIGINAL_GEOM = style([CIRCLE, POINT, LINE], LINE_STYLE);
+const ORIGINAL_GEOM = style([CIRCLE, POINT, LINE, ...CHIP], LINE_STYLE);
 const REFLECTED_GEOM = style(
-  [REFLECTED_POINT, REFLECTED_CIRCLE],
+  [REFLECTED_POINT, REFLECTED_CIRCLE, ...REFLECTED_CHIP],
   REFLECTED_STYLE,
 );
-const INVERTED_GEOM = style([INVERTED_LINE, INVERTED_POINT], INVERTED_STYLE);
+const INVERTED_GEOM = style(
+  [INVERTED_LINE, INVERTED_POINT, ...INVERTED_CHIP],
+  INVERTED_STYLE,
+);
 
 const CGA_GEOM = group(ORIGINAL_GEOM, REFLECTED_GEOM, INVERTED_GEOM);
 
