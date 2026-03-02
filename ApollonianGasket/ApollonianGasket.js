0857f4b8e6e35c781d23eb7be59fe694b503e675
Peter Gagliardi <ptrgags@gmail.com>
2026-03-01 22:01:28 -0500

    Set up empty sketch

---
+++ ApollonianGasket/ApollonianGasket.js
@@ -0,0 +1,16 @@
+import { WIDTH, HEIGHT } from "../sketchlib/dimensions.js";
+
+export const sketch = (p) => {
+  p.setup = () => {
+    p.createCanvas(
+      WIDTH,
+      HEIGHT,
+      undefined,
+      document.getElementById("sketch-canvas")
+    );
+  };
+
+  p.draw = () => {
+    p.background(0);
+  };
+};
