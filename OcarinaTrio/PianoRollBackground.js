8a71f021f39f31813841e748ca455123c21b3ff2
Peter Gagliardi <ptrgags@gmail.com>
2026-03-03 19:11:27 +0000

    Draw a piano roll background

--- OcarinaTrio/PianoRollBackground.js
+++ OcarinaTrio/PianoRollBackground.js
@@ -2,11 +2,20 @@ import { Animated } from "../sketchlib/animation/Animated.js";
 import { Color } from "../sketchlib/Color.js";
 import { HEIGHT, WIDTH } from "../sketchlib/dimensions.js";
 import { mod } from "../sketchlib/mod.js";
+import { MIDIPitch } from "../sketchlib/music/MIDIPitch.js";
+import { Oklch } from "../sketchlib/Oklch.js";
+import { Direction } from "../sketchlib/pga2d/Direction.js";
 import { Point } from "../sketchlib/pga2d/Point.js";
 import { LinePrimitive } from "../sketchlib/primitives/LinePrimitive.js";
-import { Primitive } from "../sketchlib/primitives/Primitive.js";
+import { RectPrimitive } from "../sketchlib/primitives/RectPrimitive.js";
 import { group, style } from "../sketchlib/primitives/shorthand.js";
 import { Style } from "../sketchlib/Style.js";
+
+const COLUMN_STYLES = [
+  new Style({ fill: new Oklch(1.0, 0, 0, 0.5) }),
+  new Style({ fill: new Oklch(0.5, 0, 0, 0.5) }),
+];
+const COLUMN_STYLE_INDICES = [0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0];
 
 /**
  * @implements {Animated}
@@ -21,7 +30,22 @@ export class PianoRollBackground {
   constructor(y, velocity, pitch_range) {
     this.y = y;
     this.velocity = velocity;
-    this.pitch_range = pitch_range;
+    const [min_pitch, max_pitch] = pitch_range;
+    const num_columns = max_pitch - min_pitch + 1;
+    const column_width = WIDTH / num_columns;
+
+    const columns = [];
+    for (let i = min_pitch; i <= max_pitch; i++) {
+      const x = (i - min_pitch) * column_width;
+      const pitch_class = MIDIPitch.get_pitch_class(i);
+      const rect = new RectPrimitive(
+        new Point(x, this.y),
+        new Direction(column_width, HEIGHT - this.y),
+      );
+      const style_index = COLUMN_STYLE_INDICES[pitch_class];
+      const col_style = COLUMN_STYLES[style_index];
+      columns.push(style(rect, col_style));
+    }
 
     this.lines = style(
       [],
@@ -30,7 +54,7 @@ export class PianoRollBackground {
         width: 2,
       }),
     );
-    this.primitive = group(this.lines);
+    this.primitive = group(...columns, this.lines);
   }
 
   /**
