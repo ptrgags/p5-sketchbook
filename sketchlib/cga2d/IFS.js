c9a48603533d6b7d5134180e16fb8a690f3241b7
Peter Gagliardi <ptrgags@gmail.com>
2026-03-04 21:37:09 -0500

    Add an IFS class

---
+++ sketchlib/cga2d/IFS.js
@@ -0,0 +1,38 @@
+import { CVersor } from "./CVersor.js";
+
+/**
+ *
+ * @param {CVersor} prefix
+ * @param {CVersor[]} xforms
+ * @param {number} depth
+ * @returns {Generator<CVersor>}
+ */
+function* naive_dfs(prefix, xforms, depth) {
+  if (depth === 0) {
+    yield prefix;
+    return;
+  }
+
+  for (const xform of xforms) {
+    yield* naive_dfs(prefix.compose(xform), xforms, depth - 1);
+  }
+}
+
+/**
+ * Iterated Function System, as described in _Fractals Everywhere_ by Michael F. Barnsley
+ * This is the most basic version that does not assume any properties
+ * about the transformations.
+ */
+export class IFS {
+  /**
+   * Constructor
+   * @param {CVersor[]} xforms
+   */
+  constructor(xforms) {
+    this.xforms = xforms;
+  }
+
+  *iterate(depth) {
+    yield* naive_dfs(CVersor.IDENTITY, this.xforms, depth);
+  }
+}
