77e8a2781aac8e00e866c12b3af1eed5517f08fc
Peter Gagliardi <ptrgags@gmail.com>
2026-03-06 19:12:51 -0500

    Create a SelectAnimated class

---
+++ sketchlib/animation/SelectAnimated.js
@@ -0,0 +1,71 @@
+import { mod } from "../mod.js";
+import { GroupPrimitive } from "../primitives/GroupPrimitive.js";
+import { group } from "../primitives/shorthand.js";
+import { Animated } from "./Animated.js";
+
+/**
+ * @implements {Animated}
+ */
+export class SelectAnimated {
+  /**
+   * Constructor
+   * @param {Animated[]} animations
+   */
+  constructor(animations) {
+    this.animations = animations;
+    this.index = 0;
+
+    this.primitive = group();
+    this.#update_primitive();
+  }
+
+  /**
+   * @type {Animated | undefined}
+   */
+  get selected_animation() {
+    if (this.animations.length === 0) {
+      return undefined;
+    }
+
+    return this.animations[mod(this.index, this.animations.length)];
+  }
+
+  /**
+   * Select a specific index (looping around)
+   * @param {number} index
+   */
+  select(index) {
+    this.index = index;
+    this.#update_primitive();
+  }
+
+  /**
+   * Select the next animation (looping around)
+   */
+  next() {
+    this.index++;
+    this.#update_primitive();
+  }
+
+  /**
+   * Select the previous animation (looping around)
+   */
+  prev() {
+    this.index--;
+    this.#update_primitive();
+  }
+
+  #update_primitive() {
+    const primitive =
+      this.selected_animation?.primitive ?? GroupPrimitive.EMPTY;
+    this.primitive.regroup(primitive);
+  }
+
+  /**
+   * Update the currently selected primitive (if there is one)
+   * @param {number} time
+   */
+  update(time) {
+    this.selected_animation?.update(time);
+  }
+}
