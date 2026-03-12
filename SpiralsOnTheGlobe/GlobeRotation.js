47f599cd8eb2ced1d4f32ed671b82aa64977e9ca
Peter Gagliardi <ptrgags@gmail.com>
2026-03-12 12:57:49 +0000

    Use TransformationSequence to get the correct animation

--- SpiralsOnTheGlobe/GlobeRotation.js
+++ SpiralsOnTheGlobe/GlobeRotation.js
@@ -3,6 +3,7 @@ import { Cline } from "../sketchlib/cga2d/Cline.js";
 import { ClineArc } from "../sketchlib/cga2d/ClineArc.js";
 import { CVersor } from "../sketchlib/cga2d/CVersor.js";
 import { NullPoint } from "../sketchlib/cga2d/NullPoint.js";
+import { TransformationSequence } from "../sketchlib/cga2d/TransformationSequence.js";
 import { Color } from "../sketchlib/Color.js";
 import { is_nearly } from "../sketchlib/is_nearly.js";
 import { mod } from "../sketchlib/mod.js";
@@ -50,57 +51,15 @@ function rotate_y(t) {
   const angle = 0.5 * Math.PI * t;
   return CVersor.elliptic(Direction.DIR_X.neg(), angle);
 }
-
-const X = rotate_x(1);
-const Y = rotate_y(1);
-const Z = rotate_z(1);
-
-const YZX = Y.compose(Z).compose(X);
-console.log(YZX);
-
-/**
- * Rotate the globe in quarter turns over the course of 6 measures.
- * This does a full loop of the globe
- * @param {number} t time
- * @returns {CVersor}
- */
-function rotate_globe(t) {
-  t = mod(t, 6);
-
-  // start with the key points
-  // [+x, -x, +y, -y, inf, o]
-
-  // x^-t
-  // this maps the points to [+x, -x, o, inf, +y, -y]
-  if (t < 1) {
-    return rotate_x(t);
-  }
-
-  // z^-t * x^-1
-  // this maps points to [-y, +y, o, inf, +x, -x]
-  const x = X;
-  if (t < 2) {
-    return rotate_z(t).compose(x);
-  }
-
-  const zx = Z.compose(X);
-  if (t < 3) {
-    return rotate_y(t).compose(zx);
-  }
-
-  const yxz = Y.compose(zx);
-  if (t < 4) {
-    return rotate_x(t).compose(yxz);
-  }
-
-  const xyxz = X.compose(yxz);
-  if (t < 5) {
-    return rotate_z(t).compose(xyxz);
-  }
-
-  const zxyxz = Z.compose(xyxz);
-  return rotate_y(t).compose(zxyxz);
-}
+const ROTATE_GLOBE = new TransformationSequence([
+  rotate_x,
+  rotate_z,
+  rotate_y,
+  rotate_x,
+  rotate_z,
+  rotate_y,
+]);
+const GLOBE_PERIOD = 6;
 
 const STYLE_POLES = new Style({
   fill: Color.from_hex_code("#7f00ff"),
@@ -131,17 +90,19 @@ export class GlobeRotation {
     this.primitive = group(this.poles, this.parallels, this.meridians);
   }
 
-  update(t) {
-    const xform = this.to_screen.compose(rotate_globe(t));
+  /**
+   *
+   * @param {number} time
+   */
+  update(time) {
+    const globe_t = mod(time / GLOBE_PERIOD, 1);
+    const globe_xform = ROTATE_GLOBE.value(globe_t);
+
+    const xform = this.to_screen.compose(globe_xform);
     const south_pole = xform.transform(NullPoint.ORIGIN);
     const north_pole = xform.transform(NullPoint.INF);
     const equator = xform.transform(Cline.UNIT_CIRCLE);
     let prime_meridian;
-
-    if (is_nearly(mod(t, 1.0), 0, 0.01)) {
-      //@ts-ignore
-      console.log(rotate_globe(t).transform(NullPoint.ORIGIN).point.toString());
-    }
 
     try {
       prime_meridian = xform.transform(ClineArc.PRIME_MERIDIAN);
