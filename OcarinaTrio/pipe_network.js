7731a0cc7e6e4e8c3956ec1e3cf5a61c16f23840
Peter Gagliardi <ptrgags@gmail.com>
2026-03-17 22:46:12 +0000

    make some shorthand for vline and hline

--- OcarinaTrio/pipe_network.js
+++ OcarinaTrio/pipe_network.js
@@ -27,53 +27,63 @@ function make_arc(x, y, quadrant, direction) {
   return new ArcPrimitive(new Point(x, y), BEND_RADIUS, angles);
 }
 
+/**
+ *
+ * @param {number} y
+ * @param {number} a
+ * @param {number} b
+ * @returns {LineSegment}
+ */
+function hline(y, a, b) {
+  return new LineSegment(new Point(a, y), new Point(b, y));
+}
+
+/**
+ *
+ * @param {number} x
+ * @param {number} a
+ * @param {number} b
+ * @returns {LineSegment}
+ */
+function vline(x, a, b) {
+  return new LineSegment(new Point(x, a), new Point(x, b));
+}
+
 const COMMON_SEGMENTS = [
   make_arc(450, 200, 0, -1),
-  new LineSegment(new Point(475, 200), new Point(475, 150)),
+  vline(475, 200, 150),
   make_arc(450, 150, 3, -1),
-  new LineSegment(new Point(450, 125), new Point(50, 125)),
+  hline(125, 450, 50),
   make_arc(50, 150, 2, -1),
-  new LineSegment(new Point(25, 150), new Point(25, 175)),
+  vline(25, 150, 175),
   make_arc(50, 175, 1, -1),
-  new LineSegment(new Point(50, 200), new Point(225, 200)),
+  hline(200, 50, 225),
   make_arc(225, 175, 0, -1),
-  new LineSegment(new Point(250, 175), new Point(250, 125)),
+  vline(250, 175, 125),
 ];
 
 export const PIPE_TREE_BASS = DashedTree.from_segments([
   make_arc(125, 250, 2, 1),
-  new LineSegment(new Point(125, 225), new Point(450, 225)),
+  hline(225, 125, 450),
   ...COMMON_SEGMENTS,
 ]);
 export const PIPE_TREE_TENOR = DashedTree.from_segments([
   make_arc(275, 250, 2, 1),
-  new LineSegment(new Point(275, 225), new Point(450, 225)),
+  hline(225, 275, 450),
   ...COMMON_SEGMENTS,
-  [
-    make_arc(225, 125, 3, -1),
-    new LineSegment(new Point(225, 100), new Point(175, 100)),
-  ],
-  [new LineSegment(new Point(250, 125), new Point(250, 0))],
+  [make_arc(225, 125, 3, -1), hline(100, 225, 175)],
+  [vline(250, 125, 0)],
   [
     make_arc(275, 125, 2, 1),
-    new LineSegment(new Point(275, 100), new Point(300, 100)),
-    [
-      make_arc(300, 75, 0, -1),
-      new LineSegment(new Point(325, 75), new Point(325, 0)),
-    ],
-    [
-      new LineSegment(new Point(300, 100), new Point(350, 100)),
-      [
-        make_arc(350, 75, 0, -1),
-        new LineSegment(new Point(375, 75), new Point(375, 0)),
-      ],
-    ],
+    hline(100, 275, 300),
+    [make_arc(300, 75, 0, -1), vline(325, 75, 0)],
+    [hline(100, 300, 350), [make_arc(350, 75, 0, -1), vline(375, 75, 0)]],
   ],
 ]);
 
 export const PIPE_TREE_SOPRANO = DashedTree.from_segments([
   make_arc(425, 250, 2, 1),
-  new LineSegment(new Point(425, 225), new Point(450, 225)),
+  hline(225, 425, 450),
   ...COMMON_SEGMENTS,
 ]);
 
