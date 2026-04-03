ea591c3176d22c52274c4f26ad4f17a3de541c2a
Peter Gagliardi <ptrgags@gmail.com>
2026-04-03 16:23:40 +0000

    Attempt to generate a WAV file... almost there

--- Waveforms/Waveforms.js
+++ Waveforms/Waveforms.js
@@ -50,6 +50,133 @@ function render_waveform(samples, sample_count) {
 }
 
 /**
+ *
+ * @param {string} s
+ * @returns {number}
+ */
+function ord(s) {
+  return s.charCodeAt(0);
+}
+
+// Loosely following https://devtails.xyz/@adam/how-to-write-a-wav-file-in-javascript
+const HEADER_SIZE = 44;
+// 16-bit
+const BYTES_PER_SAMPLE = 2;
+// WAV uses little-endian, see https://en.wikipedia.org/wiki/WAV#WAV_file_header
+const LITTLE_ENDIAN = true;
+
+/**
+ * Download a generated file
+ * @param {File} file The file to downlowd
+ */
+function download_file(file) {
+  const url = URL.createObjectURL(file);
+
+  const anchor = document.createElement("a");
+  anchor.setAttribute("href", url);
+  anchor.setAttribute("download", file.name);
+  document.body.appendChild(anchor);
+  anchor.click();
+  anchor.remove();
+  URL.revokeObjectURL(url);
+}
+
+/**
+ *
+ * @param {Float32Array} samples
+ * @returns {File}
+ */
+function make_wav_file(samples) {
+  const data_length = BYTES_PER_SAMPLE * samples.length;
+  const byte_length = HEADER_SIZE + data_length;
+  const buffer = new ArrayBuffer(byte_length);
+  const data_view = new DataView(buffer);
+
+  // RIFF magic number
+  let offset = 0;
+  data_view.setUint8(offset, ord("R"));
+  data_view.setUint8(offset + 1, ord("I"));
+  data_view.setUint8(offset + 2, ord("F"));
+  data_view.setUint8(offset + 3, ord("F"));
+  offset += 4;
+
+  // file size minus 8 bytes for the RIFF header
+  data_view.setUint32(offset, byte_length - 8, LITTLE_ENDIAN);
+  offset += 4;
+
+  // WAV magic number
+  data_view.setUint8(offset, ord("W"));
+  data_view.setUint8(offset + 1, ord("A"));
+  data_view.setUint8(offset + 2, ord("V"));
+  data_view.setUint8(offset + 3, ord("E"));
+  offset += 4;
+
+  // "fmt " magic number
+  data_view.setUint8(offset, ord("f"));
+  data_view.setUint8(offset + 1, ord("m"));
+  data_view.setUint8(offset + 2, ord("t"));
+  data_view.setUint8(offset + 3, ord(" "));
+  offset += 4;
+
+  // chunk size minus the 8-byte header = 16 bytes
+  data_view.setUint32(offset, 16, LITTLE_ENDIAN);
+  offset += 4;
+
+  // format 1: PCM integer
+  // huh interesting, 2 would be float values...
+  data_view.setUint16(offset, 1, LITTLE_ENDIAN);
+  offset += 2;
+
+  // number of channels. 1 for mono
+  data_view.setUint16(offset, MONO, LITTLE_ENDIAN);
+  offset += 2;
+
+  // sample rate in cycles/second
+  data_view.setUint32(offset, SAMPLE_RATE, LITTLE_ENDIAN);
+  offset += 4;
+
+  // bytes to read per second
+  data_view.setUint32(offset, SAMPLE_RATE * BYTES_PER_SAMPLE, LITTLE_ENDIAN);
+  offset += 4;
+
+  // bytes per block = number of channels * bytes per sample
+  data_view.setUint16(offset, MONO * BYTES_PER_SAMPLE, LITTLE_ENDIAN);
+  offset += 2;
+
+  // 16 bits per sample
+  data_view.setUint16(offset, BYTES_PER_SAMPLE * 8, LITTLE_ENDIAN);
+  offset += 2;
+
+  // "data" magic number
+  data_view.setUint8(offset, ord("d"));
+  data_view.setUint8(offset + 1, ord("a"));
+  data_view.setUint8(offset + 2, ord("t"));
+  data_view.setUint8(offset + 3, ord("a"));
+  offset += 4;
+
+  data_view.setUint32(offset, data_length);
+  offset += 4;
+
+  for (const [i, value] of samples.entries()) {
+    // initial range is [-1, 1]
+    // we want to scale to the range of a 16-bit signed integer
+    // which is [-2^15 - 1, 2^15]
+
+    // I'm going to be lazy and ignore the minimum value
+    const quantized = Math.round(value * (1 << 15));
+
+    data_view.setInt16(offset + BYTES_PER_SAMPLE * i, quantized, LITTLE_ENDIAN);
+  }
+  offset += BYTES_PER_SAMPLE * samples.length;
+  if (offset !== byte_length) {
+    console.log("actual", offset, "expected", byte_length);
+    throw new Error("whoops, my offsets are off");
+  }
+
+  return new File([buffer], "waveform.wav", { type: "audio/wav" });
+}
+
+/**
  * @implements {Animated}
  */
 class WaveformsAnimation {
@@ -86,6 +213,8 @@ class WaveformsAnimation {
 
     const period = 1 / freq;
     const period_samples = Math.floor(period * SAMPLE_RATE);
+
+    download_file(make_wav_file(samples));
 
     this.primitive = style(
       render_waveform(samples, period_samples),
