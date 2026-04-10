028fa8c16d9361104cf5389e838eb7b5fc8de811
Peter Gagliardi <ptrgags@gmail.com>
2026-04-10 14:25:00 +0000

    Sketch out method for computing intersections

---
+++ XRayDiffraction/DetectorPoint.js
@@ -0,0 +1,31 @@
+import { Point } from "../sketchlib/pga2d/Point.js";
+
+export class DetectorPoint {
+  constructor() {
+    /**
+     * Map of sign(peak.y) -> peak
+     * Since the input x-ray is horizontal, each spatial frequency will be
+     * detected twice, once bending upwards, once bending downwards.
+     * Storing the sign of the y-component is enough to distingush the two
+     * peaks while avoiding duplicate detections.
+     *
+     * In the case of peaks exactly horizontal, this would give 0 as the key,
+     * that works fine as well so there will only be 1 point stored after
+     * deduplicating.
+     * @type {Map<number, Point>}
+     */
+    this.peaks = new Map();
+  }
+
+  /**
+   * Add a detected Bragg diffraction peak. This ignores duplicates.
+   * @param {Point} bragg_peak The point where the x-ray hit the detector screen
+   */
+  add_peak(bragg_peak) {
+    const key = Math.sign(bragg_peak.y);
+    if (this.peaks.has(key)) {
+      return;
+    }
+    this.peaks.set(key, bragg_peak);
+  }
+}
--- XRayDiffraction/XRayLab.js
+++ XRayDiffraction/XRayLab.js
@@ -1,16 +1,22 @@
 import { Color } from "../sketchlib/Color.js";
 import { HEIGHT, SCREEN_CENTER, WIDTH } from "../sketchlib/dimensions.js";
+import { is_nearly } from "../sketchlib/is_nearly.js";
 import { Oklch } from "../sketchlib/Oklch.js";
 import { Direction } from "../sketchlib/pga2d/Direction.js";
+import { Line } from "../sketchlib/pga2d/Line.js";
 import { Point } from "../sketchlib/pga2d/Point.js";
 import { Motor } from "../sketchlib/pga2d/versors.js";
 import { Circle } from "../sketchlib/primitives/Circle.js";
 import { LineSegment } from "../sketchlib/primitives/LineSegment.js";
 import { PolygonPrimitive } from "../sketchlib/primitives/PolygonPrimitive.js";
 import { group, style } from "../sketchlib/primitives/shorthand.js";
+import { VectorPrimitive } from "../sketchlib/primitives/VectorPrimitive.js";
 import { Style } from "../sketchlib/Style.js";
+import { DetectorPoint } from "./DetectorPoint.js";
 import { LatticeVector } from "./LatticeVector.js";
 import { XRaySimulation } from "./XRaySimulation.js";
+
+const EPSILON = 1e-8;
 
 const ORIGIN = new Point(WIDTH / 2, HEIGHT / 4);
 
@@ -67,10 +73,15 @@ const GONIOMETER_FRAME = style(
 );
 
 const DETECTOR_X = 350;
-const DETECTOR_LINE = style(
+const DETECTOR_SEGMENT = style(
   new LineSegment(new Point(DETECTOR_X, 0), new Point(DETECTOR_X, HEIGHT / 2)),
   STYLE_MACHINE_LINES,
 );
+const DETECTOR_LINE = new Line(1, 0, DETECTOR_X);
+
+const STYLE_DETECTED = new Style({
+  fill: new Oklch(0.7, 0.1, 150),
+});
 
 // radius of the crystal from center to the corner in pixels
 const CRYSTAL_RADIUS = 20;
@@ -92,17 +103,23 @@ export class XRayLab {
       this.#update_rays(angle, newly_detected);
     });
 
+    /**
+     * @type {Map<LatticeVector, DetectorPoint>}
+     */
+    this.detected_peaks = new Map();
+
     this.outgoing_rays = style([], STYLE_XRAY);
     this.crystal = style([], STYLE_CRYSTAL);
     this.goniometer_bar = style([], STYLE_MACHINE_LINES);
+    this.peaks = style([], STYLE_DETECTED);
     this.primitive = group(
       GONIOMETER_FRAME,
       this.goniometer_bar,
       INCOMING_BEAM,
       this.outgoing_rays,
       EMITTER,
-      DETECTOR_LINE,
-
+      DETECTOR_SEGMENT,
+      this.peaks,
       this.crystal,
     );
   }
@@ -158,5 +175,52 @@ export class XRayLab {
       return new LineSegment(ORIGIN, ORIGIN.add(dir_out));
     });
     this.outgoing_rays.regroup(...outgoing_rays);
+  }
+
+  /**
+   *
+   * @param {Direction} k_in Incoming wavevector in cycles/angstrom
+   * @param {LatticeVector} g_hk Detected lattice vector
+   * @param {Motor} rotation Rotation motor for this frame
+   * @returns {LineSegment | VectorPrimitive | undefined}
+   */
+  #pew_pew(k_in, g_hk, rotation) {
+    const rotated_g = rotation.transform_dir(g_hk.wavevector);
+
+    // k_out is the diffracted wavevector
+    // g = k_out - k_in, so
+    // k_out = k_in + g
+    const k_out = k_in.add(rotated_g);
+
+    // Will the scattered ray hit the detector? only rays moving to the right
+    // will even have a chance of this happening
+    if (k_out.x > EPSILON) {
+      const trajectory = ORIGIN.join(k_out);
+      const intersection = trajectory.meet(DETECTOR_LINE);
+
+      // Here we only want points that are visible in the top half of the screen
+      if (
+        intersection instanceof Point &&
+        intersection.y >= 0 &&
+        intersection.y < HEIGHT / 2
+      ) {
+        const detector_point = this.detected_peaks.getOrInsertComputed(
+          g_hk,
+          () => new DetectorPoint(),
+        );
+        detector_point.add_peak(intersection);
+
+        return new LineSegment(ORIGIN, intersection);
+      }
+    }
+
+    // The ray either doesn't intersect the detector, or intersects offscreen.
+    // either way, draw it as a direction vector instead
+    if (k_out.equals(Direction.ZERO)) {
+      return new LineSegment(ORIGIN, ORIGIN);
+    }
+
+    const tip = k_out.set_length(OUTGOING_RAY_LENGTH).to_point();
+    return new LineSegment(ORIGIN, tip);
   }
 }
