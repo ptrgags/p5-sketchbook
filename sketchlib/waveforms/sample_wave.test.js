da0360f4b769f45798f4c9266da1b4eff73c7d5c
Peter Gagliardi <ptrgags@gmail.com>
2026-04-14 15:17:52 +0000

    Rephrase sample_single_cycle as sample_wave

--- sketchlib/waveforms/sample_wave.test.js
+++ sketchlib/waveforms/sample_wave.test.js
@@ -1,13 +1,22 @@
 import { describe, it, expect } from "vitest";
-import { sample_single_cycle } from "./sample_single_cycle.js";
 import { sawtooth, sine } from "./basic_waves.js";
 import { SAMPLE_RATE } from "./encode_wav.js";
 import { expect_arrays } from "../test_helpers/expect_arrays.js";
+import { sample_n_cycles, sample_wave } from "./sample_wave.js";
 
-describe("sample_single_cycle", () => {
+describe("sample_wave", () => {
+  it("Produces a buffer with the correct number of samples", () => {
+    const result = sample_wave(sine, 440, 250).length;
+
+    const expected = 250;
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("sample_n_cycles", () => {
   it("with zero frequency throws error", () => {
     expect(() => {
-      return sample_single_cycle(sine, 0);
+      return sample_n_cycles(sine, 0, 1);
     }).toThrowError("frequency must be positive");
   });
 
@@ -15,7 +24,7 @@ describe("sample_single_cycle", () => {
     const sample_count = 4;
     const freq = SAMPLE_RATE / sample_count;
 
-    const result = sample_single_cycle(sine, freq);
+    const result = sample_n_cycles(sine, freq, 1);
 
     // sampling a sine wave with 4 samples should produce the points at
     // 0, pi/2, pi, 3pi/2 (but not 2pi! that's the first sample of the next cycle)
@@ -27,7 +36,7 @@ describe("sample_single_cycle", () => {
     // A4 = 440 Hz
     const freq = 440;
 
-    const result = sample_single_cycle(sawtooth, freq);
+    const result = sample_n_cycles(sawtooth, freq, 1);
 
     // 44100 / 440 = 100.227, this rounds down to 100
     const expected = 100;
@@ -38,7 +47,7 @@ describe("sample_single_cycle", () => {
     const sample_count = 12;
     const freq = SAMPLE_RATE / sample_count;
 
-    const result = sample_single_cycle(sawtooth, freq);
+    const result = sample_n_cycles(sawtooth, freq, 1);
 
     // This sawtooth wave ramps from 1 to -1 over 12 samples, something like:
     // lerp(1, -1, i/12)
@@ -60,5 +69,19 @@ describe("sample_single_cycle", () => {
       1 - 11 / 6,
     ]);
     expect(result).toEqual(expected);
+  });
+
+  it("with multiple cycles repeats the array entries", () => {
+    const sample_count = 4;
+    const freq = SAMPLE_RATE / sample_count;
+
+    const result = sample_n_cycles(sine, freq, 4);
+
+    // sampling a sine wave with 4 samples should produce the points at
+    // 0, pi/2, pi, 3pi/2 (but not 2pi! that's the first sample of the next cycle)
+    const expected = new Float32Array([
+      0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1,
+    ]);
+    expect_arrays(result, expected, (r, e) => expect(r).toBeCloseTo(e));
   });
 });
