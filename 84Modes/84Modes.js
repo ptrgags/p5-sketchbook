4c24c1f6f7c8df61bdf62aa956b475a31121ed6a
Peter Gagliardi <ptrgags@gmail.com>
2026-04-28 13:44:45 +0000

    Set up the big circle

--- 84Modes/84Modes.js
+++ 84Modes/84Modes.js
@@ -1,11 +1,17 @@
-import { WIDTH, HEIGHT } from "../sketchlib/dimensions.js";
+import { WIDTH, HEIGHT, SCREEN_CENTER } from "../sketchlib/dimensions.js";
 import { CanvasMouseHandler } from "../sketchlib/input/CanvasMouseHandler.js";
 import { PlayButtonScene } from "../sketchlib/scenes/PlayButtonScene.js";
 import { SoundManager } from "../sketchlib/SoundManager.js";
 import { Animated } from "../sketchlib/animation/Animated.js";
 import { SoundScene } from "../sketchlib/scenes/SoundScene.js";
 import { MouseCallbacks } from "../sketchlib/input/MouseCallbacks.js";
-import { group } from "../sketchlib/primitives/shorthand.js";
+import { group, style } from "../sketchlib/primitives/shorthand.js";
+import { Style } from "../sketchlib/Style.js";
+import { Color } from "../sketchlib/Color.js";
+import { Circle } from "../sketchlib/primitives/Circle.js";
+import { Point } from "../sketchlib/pga2d/Point.js";
+import { Direction } from "../sketchlib/pga2d/Direction.js";
+import { LineSegment } from "../sketchlib/primitives/LineSegment.js";
 
 const MOUSE = new CanvasMouseHandler();
 
@@ -18,12 +24,41 @@ const SOUND_MANIFEST = {
 //@ts-ignore
 const SOUND = new SoundManager(Tone, SOUND_MANIFEST);
 
+const STYLE_LINES = new Style({
+  stroke: Color.WHITE,
+  width: 4,
+});
+
+const CIRCLE_RADIUS = 240;
+const MODE_CIRCLE = new Circle(SCREEN_CENTER, CIRCLE_RADIUS);
+
+const MAJOR_TICK_LENGTH = 32;
+const MAJOR_TICKS = Direction.roots_of_unity(12).map((dir) => {
+  return new LineSegment(
+    SCREEN_CENTER.add(dir.scale(CIRCLE_RADIUS - MAJOR_TICK_LENGTH / 2)),
+    SCREEN_CENTER.add(dir.scale(CIRCLE_RADIUS + MAJOR_TICK_LENGTH / 2)),
+  );
+});
+
+const MINOR_TICK_LENGTH = 16;
+const MINOR_TICKS = Direction.roots_of_unity(84).map((dir) => {
+  return new LineSegment(
+    SCREEN_CENTER.add(dir.scale(CIRCLE_RADIUS - MINOR_TICK_LENGTH / 2)),
+    SCREEN_CENTER.add(dir.scale(CIRCLE_RADIUS + MINOR_TICK_LENGTH / 2)),
+  );
+});
+
+const CIRCLE_OF_MODES = style(
+  [MODE_CIRCLE, ...MINOR_TICKS, ...MAJOR_TICKS],
+  STYLE_LINES,
+);
+
 /**
  * @implements {Animated}
  */
-class TEMPLATEAnimation {
+class Modes84Animation {
   constructor() {
-    this.primitive = group();
+    this.primitive = group(CIRCLE_OF_MODES);
   }
 
   /**
@@ -33,7 +68,11 @@ class TEMPLATEAnimation {
     return [];
   }
 
-  update() {}
+  /**
+   *
+   * @param {number} time
+   */
+  update(time) {}
 }
 
 //@ts-ignore
@@ -52,7 +91,7 @@ export const sketch = (p) => {
     MOUSE.callbacks = scene.mouse_callbacks;
 
     scene.events.addEventListener("scene-change", () => {
-      scene = new SoundScene(SOUND, new TEMPLATEAnimation());
+      scene = new SoundScene(SOUND, new Modes84Animation());
       MOUSE.callbacks = scene.mouse_callbacks;
     });
   };
