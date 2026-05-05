61bf6bc94d42a90f3c1f20d7ab7f48e0134ff814
Peter Gagliardi <ptrgags@gmail.com>
2026-05-05 17:04:04 +0000

    Add a toggle button and a description

--- GuitarHarmonics/GuitarHarmonics.js
+++ GuitarHarmonics/GuitarHarmonics.js
@@ -1,4 +1,5 @@
 import { WIDTH, HEIGHT } from "../sketchlib/dimensions.js";
+import { expect_element } from "../sketchlib/dom/expect_element.js";
 import { ShowHidePrimitive } from "../sketchlib/primitives/ShowHidePrimitive.js";
 import { HarmonicFinder } from "./HarmonicFinder.js";
 import { InfiniteHarmonics } from "./InfiniteHarmonics.js";
@@ -17,6 +18,17 @@ export const sketch = (p) => {
       undefined,
       document.getElementById("sketch-canvas"),
     );
+    expect_element("toggle-visualization", HTMLButtonElement).addEventListener(
+      "click",
+      () => {
+        SCENE.show_flags.map((x) => !x);
+
+        // Redraw the scene.
+        background(0);
+        SCENE.draw(p);
+      },
+    );
+
     p.noLoop();
 
     p.background(0);
