1eee348d9842e757f1eecdf53df571226644af4b
Peter Gagliardi <ptrgags@gmail.com>
2026-06-04 10:39:36 -0400

    Tweak the sizing to match what I want to export for 3D printing

--- SpeechBubble/SpeechBubble.js
+++ SpeechBubble/SpeechBubble.js
@@ -11,6 +11,31 @@ import { SpeechBubblePrimitive } from "../sketchlib/primitives/SpeechBubblePrimi
 import { Style } from "../sketchlib/Style.js";
 import { encode_svg_file } from "../sketchlib/svg/encode_svg.js";
 
+// I want to 3D print this, the width I want is 2.5 cm, or 25 mm
+const MAX_WIDTH = 25;
+
+const BUBBLE_PRINT = new SpeechBubblePrimitive(
+  Rect.from_center(Point.ORIGIN, new Direction(MAX_WIDTH, MAX_WIDTH / 2)),
+  Direction.ZERO,
+  new Point(-MAX_WIDTH / 2, MAX_WIDTH / 4),
+  1,
+);
+
+// Scale up things for the screen
+const PX_PER_MM = 10;
+const BUBBLE_SCREEN = new SpeechBubblePrimitive(
+  Rect.from_center(
+    SCREEN_CENTER,
+    new Direction(MAX_WIDTH, MAX_WIDTH / 2).scale(PX_PER_MM),
+  ),
+  Direction.ZERO,
+  new Point(
+    SCREEN_CENTER.x - (MAX_WIDTH / 2) * PX_PER_MM,
+    SCREEN_CENTER.y + (MAX_WIDTH / 4) * PX_PER_MM,
+  ),
+  1 * PX_PER_MM,
+);
+
 const BUBBLE = new SpeechBubblePrimitive(
   Rect.from_center(SCREEN_CENTER, new Direction(150, 50)),
   new Direction(50, 50),
@@ -18,33 +43,55 @@ const BUBBLE = new SpeechBubblePrimitive(
   8,
 );
 
-const STRIDE = Direction.DIR_X.scale(50);
+const ELLIPSIS_STRIDE_PRINT = Direction.DIR_X.scale(6);
+const ELLIPSIS_RADIUS_PRINT = 1.25;
+const ELLIPSIS_PRINT = group(
+  new Circle(
+    Point.ORIGIN.add(ELLIPSIS_STRIDE_PRINT.neg()),
+    ELLIPSIS_RADIUS_PRINT,
+  ),
+  new Circle(Point.ORIGIN, ELLIPSIS_RADIUS_PRINT),
+  new Circle(Point.ORIGIN.add(ELLIPSIS_STRIDE_PRINT), ELLIPSIS_RADIUS_PRINT),
+);
 
-const ELLIPSIS_RADIUS = 10;
-const ELLIPSIS = group(
-  new Circle(SCREEN_CENTER.add(STRIDE.neg()), ELLIPSIS_RADIUS),
-  new Circle(SCREEN_CENTER, ELLIPSIS_RADIUS),
-  new Circle(SCREEN_CENTER.add(STRIDE), ELLIPSIS_RADIUS),
+const ELLIPSIS_STRIDE_SCREEN = ELLIPSIS_STRIDE_PRINT.scale(PX_PER_MM);
+const ELLIPSIS_RADIUS_SCREEN = ELLIPSIS_RADIUS_PRINT * PX_PER_MM;
+const ELLIPSIS_SCREEN = group(
+  new Circle(
+    SCREEN_CENTER.add(ELLIPSIS_STRIDE_SCREEN.neg()),
+    ELLIPSIS_RADIUS_SCREEN,
+  ),
+  new Circle(SCREEN_CENTER, ELLIPSIS_RADIUS_SCREEN),
+  new Circle(SCREEN_CENTER.add(ELLIPSIS_STRIDE_SCREEN), ELLIPSIS_RADIUS_SCREEN),
 );
 
 const STYLE_ELLIPSIS = new Style({
   fill: Color.BLACK,
 });
 
-const SCENE = group(BUBBLE, style(ELLIPSIS, STYLE_ELLIPSIS));
+const SCENE = group(BUBBLE_SCREEN, style(ELLIPSIS_SCREEN, STYLE_ELLIPSIS));
 
 const STYLE_OUTLINE = new Style({ fill: Color.BLACK });
 const STYLE_INTERIOR = new Style({ fill: Color.WHITE });
 const SVG_SCENE = group(
-  style(BUBBLE.outer_primitiive, STYLE_OUTLINE),
-  style(BUBBLE.inner_primitiive, STYLE_INTERIOR),
-  style(ELLIPSIS, STYLE_ELLIPSIS),
+  style(BUBBLE_PRINT.outer_primitiive, STYLE_OUTLINE),
+  style(BUBBLE_PRINT.inner_primitiive, STYLE_INTERIOR),
+  style(ELLIPSIS_PRINT, STYLE_ELLIPSIS),
+);
+
+const SVG_VIEWPORT = Rect.from_center(
+  Point.ORIGIN,
+  new Direction(MAX_WIDTH, MAX_WIDTH),
 );
 
 const SLASH = new KeywordRecognizer();
 // export SVG version of ellipse
 SLASH.register(["Slash", "KeyS", "KeyV", "KeyG"], () => {
-  const svg_file = encode_svg_file(SVG_SCENE, "speech-bubble.svg");
+  const svg_file = encode_svg_file(
+    SVG_SCENE,
+    SVG_VIEWPORT,
+    "speech-bubble.svg",
+  );
   download_file(svg_file);
 });
 
--- sketchlib/svg/encode_svg.js
+++ sketchlib/svg/encode_svg.js
@@ -1,12 +1,20 @@
+import { Rect } from "../primitives/Rect.js";
 import { ToSVG } from "./ToSVG.js";
 
 /**
  * Encode an object as an SVG tag
  * @param {ToSVG} svg_primitive An object that can be turned into SVG tags
+ * @param {Rect} view_box The bounding box to view
  * @returns {SVGSVGElement} SVG element
  */
-export function encode_svg(svg_primitive) {
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+export function encode_svg(svg_primitive, view_box) {
+  const namespace = "http://www.w3.org/2000/svg";
+  const svg = document.createElementNS(namespace, "svg");
+
+  const { x, y } = view_box.position;
+  const { x: w, y: h } = view_box.dimensions;
+  svg.setAttribute("viewBox", `${x} ${y} ${w} ${h}`);
+
   const content = svg_primitive.to_svg();
   svg.appendChild(content);
   return svg;
@@ -15,16 +23,17 @@ export function encode_svg(svg_primitive) {
 /**
  * Encode an object to an SVG tag, then convert it to a File object for
  * downloading
- * @param {ToSVG} svg_primitive
+ * @param {ToSVG} svg_primitive The primitive to convert to SVG
+ * @param {Rect} view_box The bounding box to view
  * @param {string} filename A filename that must end in .svg
  * @returns {File} A file for downloading
  */
-export function encode_svg_file(svg_primitive, filename) {
+export function encode_svg_file(svg_primitive, view_box, filename) {
   if (!filename.endsWith(".svg")) {
     throw new Error("filename must end with .svg");
   }
 
-  const svg = encode_svg(svg_primitive);
+  const svg = encode_svg(svg_primitive, view_box);
   const serializer = new XMLSerializer();
   const svg_str = serializer.serializeToString(svg);
 
