1234712ce772c9d3a518ab11b06512549a0e7b42
Peter Gagliardi <ptrgags@gmail.com>
2026-06-26 11:43:42 -0400

    Sketch out a very basic clock

---
+++ BasicClock/BasicClock.js
@@ -0,0 +1,89 @@
+import { WIDTH, HEIGHT, SCREEN_CENTER } from "../sketchlib/dimensions.js";
+import { Direction } from "../sketchlib/pga2d/Direction.js";
+import { Circle } from "../sketchlib/primitives/Circle.js";
+import { LineSegment } from "../sketchlib/primitives/LineSegment.js";
+import { group } from "../sketchlib/primitives/shorthand.js";
+import { VectorPrimitive } from "../sketchlib/primitives/VectorPrimitive.js";
+
+const DIAL_RADIUS = 200;
+const HASH_LENGTH = 50;
+const TICK_MARKS = Direction.roots_of_unity(12).map((dir) => {
+  const outer_point = SCREEN_CENTER.add(dir.scale(DIAL_RADIUS));
+  const inner_point = SCREEN_CENTER.add(dir.scale(DIAL_RADIUS - HASH_LENGTH));
+  return new LineSegment(outer_point, inner_point);
+});
+
+const DIAL = new Circle(SCREEN_CENTER, DIAL_RADIUS);
+
+const SECOND_HAND = new VectorPrimitive(
+  SCREEN_CENTER,
+  SCREEN_CENTER.add(Direction.from_angle(-Math.PI / 2).scale(DIAL_RADIUS)),
+);
+
+const MINUTE_HAND = new VectorPrimitive(
+  SCREEN_CENTER,
+  SCREEN_CENTER.add(
+    Direction.from_angle(-Math.PI / 2).scale(DIAL_RADIUS * 0.9),
+  ),
+);
+
+const HOUR_HAND = new VectorPrimitive(
+  SCREEN_CENTER,
+  SCREEN_CENTER.add(
+    Direction.from_angle(-Math.PI / 2).scale(DIAL_RADIUS * 0.5),
+  ),
+);
+
+const SCENE = group(
+  DIAL,
+  group(...TICK_MARKS),
+  HOUR_HAND,
+  MINUTE_HAND,
+  SECOND_HAND,
+);
+
+/**
+ *
+ * @param {Date} date
+ */
+function update_hands(date) {
+  const hours = date.getHours();
+  const hour_angle = -Math.PI / 2 + ((hours % 12) * Math.PI) / 6;
+  HOUR_HAND.tip = SCREEN_CENTER.add(
+    Direction.from_angle(hour_angle).scale(DIAL_RADIUS * 0.5),
+  );
+
+  const minutes = date.getMinutes();
+  const min_angle = -Math.PI / 2 + ((minutes % 60) * Math.PI) / 30;
+  MINUTE_HAND.tip = SCREEN_CENTER.add(
+    Direction.from_angle(min_angle).scale(DIAL_RADIUS * 0.9),
+  );
+
+  const seconds = date.getSeconds();
+  const sec_angle = -Math.PI / 2 + ((seconds % 60) * Math.PI) / 30;
+  SECOND_HAND.tip = SCREEN_CENTER.add(
+    Direction.from_angle(sec_angle).scale(DIAL_RADIUS),
+  );
+}
+
+// @ts-ignore
+export const sketch = (p) => {
+  p.setup = () => {
+    p.createCanvas(
+      WIDTH,
+      HEIGHT,
+      undefined,
+      document.getElementById("sketch-canvas"),
+    );
+  };
+
+  p.draw = () => {
+    p.background(0);
+
+    const date = new Date();
+
+    update_hands(date);
+
+    SCENE.draw(p);
+  };
+};
