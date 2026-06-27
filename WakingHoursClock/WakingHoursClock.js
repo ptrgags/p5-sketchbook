692231f9a393eda5476c67b7004331d441a84039
Peter Gagliardi <ptrgags@gmail.com>
2026-06-26 20:29:08 -0400

    Add numerals

--- WakingHoursClock/WakingHoursClock.js
+++ WakingHoursClock/WakingHoursClock.js
@@ -4,19 +4,40 @@ import { Color } from "../sketchlib/Color.js";
 import { WIDTH, HEIGHT, SCREEN_CENTER } from "../sketchlib/dimensions.js";
 import { Direction } from "../sketchlib/pga2d/Direction.js";
 import { ArcPrimitive } from "../sketchlib/primitives/ArcPrimitive.js";
+import { GroupPrimitive } from "../sketchlib/primitives/GroupPrimitive.js";
 import { LineSegment } from "../sketchlib/primitives/LineSegment.js";
 import { group, style } from "../sketchlib/primitives/shorthand.js";
+import { TextPrimitive } from "../sketchlib/primitives/TextPrimitive.js";
+import { TextStyle } from "../sketchlib/primitives/TextStyle.js";
 import { Style } from "../sketchlib/Style.js";
 
-const DIAL_RADIUS = 200;
+const DIAL_RADIUS = 175;
 const HAND_LENGTH = 225;
+const HASH_LENGTH = 30;
+const NUMERAL_RADIUS = 235;
 
-const HASH_LENGTH = 25;
 const TICK_MARKS = Direction.roots_of_unity(24).map((dir) => {
   const outer_point = SCREEN_CENTER.add(dir.scale(DIAL_RADIUS));
   const inner_point = SCREEN_CENTER.add(dir.scale(DIAL_RADIUS + HASH_LENGTH));
   return new LineSegment(outer_point, inner_point);
 });
+
+const MINOR_TICKS = Direction.roots_of_unity(24 * 4).map((dir) => {
+  const outer_point = SCREEN_CENTER.add(dir.scale(DIAL_RADIUS));
+  const inner_point = SCREEN_CENTER.add(
+    dir.scale(DIAL_RADIUS + HASH_LENGTH * 0.5),
+  );
+  return new LineSegment(outer_point, inner_point);
+});
+
+const NUMERALS = Direction.roots_of_unity(24).map((dir, i) => {
+  const numeral = (i + 6) % 24;
+  return new TextPrimitive(
+    `${numeral}`,
+    SCREEN_CENTER.add(dir.scale(NUMERAL_RADIUS)),
+  );
+});
+const TEXT_STYLE_NUMERALS = new TextStyle(25, "center", "center");
 
 const HOUR_WAKE = 6;
 const HOUR_SLEEP = 22;
@@ -40,6 +61,15 @@ const STYLE_TICKS = new Style({
   width: 4,
 });
 
+const STYLE_MINOR_TICKS = new Style({
+  stroke: Color.WHITE,
+  width: 2,
+});
+
+const STYLE_NUMERALS = new Style({
+  fill: Color.WHITE,
+});
+
 const STYLE_WAKE = new Style({
   stroke: Color.CYAN,
   width: 8,
@@ -55,7 +85,12 @@ const STYLE_HAND = new Style({
 });
 
 const SCENE = group(
+  style(MINOR_TICKS, STYLE_MINOR_TICKS),
   style(TICK_MARKS, STYLE_TICKS),
+  new GroupPrimitive(NUMERALS, {
+    style: STYLE_NUMERALS,
+    text_style: TEXT_STYLE_NUMERALS,
+  }),
   style(ARC_SLEEP, STYLE_SLEEP),
   style(ARC_WAKE, STYLE_WAKE),
   style(HAND, STYLE_HAND),
