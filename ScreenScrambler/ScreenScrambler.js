498129a0500154569b9ff44e1ae80bd0bf19051c
Peter Gagliardi <ptrgags@gmail.com>
2026-07-12 17:02:45 -0400

    start setting up the scene

--- ScreenScrambler/ScreenScrambler.js
+++ ScreenScrambler/ScreenScrambler.js
@@ -4,7 +4,10 @@ import { WIDTH, HEIGHT } from "../sketchlib/dimensions.js";
 import { Oklch } from "../sketchlib/Oklch.js";
 import { Direction } from "../sketchlib/pga2d/Direction.js";
 import { Point } from "../sketchlib/pga2d/Point.js";
+import { Drawing } from "../sketchlib/pixel/Drawing.js";
+import { Tilemap } from "../sketchlib/pixel/Tilemap.js";
 import { Circle } from "../sketchlib/primitives/Circle.js";
+import { Rect } from "../sketchlib/primitives/Rect.js";
 import { group, style } from "../sketchlib/primitives/shorthand.js";
 import { Random } from "../sketchlib/random.js";
 import { range } from "../sketchlib/range.js";
@@ -35,12 +38,25 @@ const SHAPES = range(20)
     );
   })
   .toArray();
-const SCENE = group(...SHAPES);
 
 const CLOCK = new Clock();
 
+const TILE_SIZE = new Direction(100, 100);
+
+const SCENE_OFFSCREEN = group(...SHAPES);
+
+const CURSOR = new Rect(Point.ORIGIN, TILE_SIZE);
+const STYLED_CURSOR = style(CURSOR, Style.lines(Color.YELLOW, 4));
+
+// will be populated
+const SCENE_SCREEN = group(SCENE_OFFSCREEN, STYLED_CURSOR);
+
 // @ts-ignore
 export const sketch = (p) => {
+  let drawing_offscreen;
+  let tilemap_copy;
+  let tilemap_paste;
+
   p.setup = () => {
     p.createCanvas(
       WIDTH,
@@ -48,12 +64,34 @@ export const sketch = (p) => {
       undefined,
       document.getElementById("sketch-canvas"),
     );
+
+    const gfx_offscreen = p.createGraphics(WIDTH, HEIGHT);
+    drawing_offscreen = new Drawing(gfx_offscreen, Point.ORIGIN);
+    tilemap_copy = new Tilemap(
+      p,
+      gfx_offscreen,
+      TILE_SIZE,
+      new Direction(1, 1),
+      Point.ORIGIN,
+    );
+    tilemap_paste = new Tilemap(
+      p,
+      gfx_offscreen,
+      TILE_SIZE,
+      new Direction(5, 7),
+      Point.ORIGIN,
+    );
+
+    // set up a feedback loop - pasting to the screen impacts future copy
+    // operations!
+    SCENE_OFFSCREEN.regroup(tilemap_paste, ...SHAPES);
+    SCENE_SCREEN.regroup(SCENE_OFFSCREEN, STYLED_CURSOR, tilemap_copy);
   };
 
   p.draw = () => {
     p.background(0);
 
-    SCENE.draw(p);
+    SCENE_SCREEN.draw(p);
 
     const time = CLOCK.elapsed_time;
     for (const shape of SHAPES) {
