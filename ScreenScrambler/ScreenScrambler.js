cafb380b921db83f23ea311f367e398014ac3d5a
Peter Gagliardi <ptrgags@gmail.com>
2026-07-12 17:42:47 -0400

    Animate the cursor

--- ScreenScrambler/ScreenScrambler.js
+++ ScreenScrambler/ScreenScrambler.js
@@ -1,6 +1,8 @@
 import { Clock } from "../sketchlib/animation/Clock.js";
 import { Color } from "../sketchlib/Color.js";
 import { WIDTH, HEIGHT } from "../sketchlib/dimensions.js";
+import { Ease } from "../sketchlib/Ease.js";
+import { Index2D } from "../sketchlib/Grid.js";
 import { Oklch } from "../sketchlib/Oklch.js";
 import { Direction } from "../sketchlib/pga2d/Direction.js";
 import { Point } from "../sketchlib/pga2d/Point.js";
@@ -12,6 +14,7 @@ import { group, style } from "../sketchlib/primitives/shorthand.js";
 import { Random } from "../sketchlib/random.js";
 import { range } from "../sketchlib/range.js";
 import { Style } from "../sketchlib/Style.js";
+import { Tween } from "../sketchlib/Tween.js";
 import { FlyingShape } from "./FlyingShape.js";
 
 const SHAPES = range(20)
@@ -43,19 +46,168 @@ const CLOCK = new Clock();
 
 const TILE_SIZE = new Direction(100, 100);
 
+// These will be updated in setup() as some primitives need p5 resources
 const SCENE_OFFSCREEN = group(...SHAPES);
+const SCENE_SCREEN = group(SCENE_OFFSCREEN);
 
-const CURSOR = new Rect(Point.ORIGIN, TILE_SIZE);
-const STYLED_CURSOR = style(CURSOR, Style.lines(Color.YELLOW, 4));
+function rand_index() {
+  const rand_row = Random.rand_int(0, 7);
+  const rand_col = Random.rand_int(0, 5);
+  return new Index2D(rand_row, rand_col);
+}
 
-// will be populated
-const SCENE_SCREEN = group(SCENE_OFFSCREEN, STYLED_CURSOR);
+/**
+ * @enum {number}
+ */
+const CopyPasteState = {
+  MOVING_COPY: 0,
+  COPY: 1,
+  MOVING_PASTE: 2,
+  PASTE: 3,
+};
+
+const DURATION_MOVE = 2.0;
+
+const STYLE_COPY = Style.lines(Color.YELLOW, 4);
+const STYLE_PASTE = Style.lines(Color.BLUE, 4);
+
+class CopyPaste {
+  /**
+   *
+   * @param {Tilemap} tilemap_copy
+   * @param {Tilemap} tilemap_paste
+   */
+  constructor(tilemap_copy, tilemap_paste) {
+    this.tilemap_copy = tilemap_copy;
+    this.tilemap_paste = tilemap_paste;
+
+    this.state = CopyPasteState.MOVING_COPY;
+
+    this.cursor = new Rect(Point.ORIGIN, TILE_SIZE);
+    this.styled_cursor = style(this.cursor, STYLE_COPY);
+
+    this.next_index = rand_index();
+    this.next_target = TILE_SIZE.mul_components(
+      new Direction(this.next_index.j, this.next_index.i),
+    ).to_point();
+
+    this.next_paste_index = 0;
+
+    this.tween = Tween.point(
+      this.cursor.position,
+      this.next_target,
+      0,
+      DURATION_MOVE,
+      Ease.in_out_cubic,
+    );
+
+    this.primitive = group(this.styled_cursor);
+  }
+
+  /**
+   *
+   * @param {number} time
+   * @returns
+   */
+  move(time) {
+    const position = this.tween.get_value(time);
+    const is_done = this.tween.is_done(time);
+
+    if (is_done && this.state === CopyPasteState.MOVING_COPY) {
+      this.state = CopyPasteState.COPY;
+      return;
+    } else if (is_done && this.state === CopyPasteState.MOVING_PASTE) {
+      this.state = CopyPasteState.PASTE;
+      return;
+    }
+
+    this.cursor.position = position;
+  }
+
+  /**
+   *
+   * @param {number} time
+   */
+  copy(time) {
+    // Select the next paste location in the list
+    const row = Math.floor(this.next_paste_index / 5);
+    const col = this.next_paste_index % 5;
+    this.next_index = new Index2D(row, col);
+    this.next_paste_index++;
+    this.choose_next_target(time);
+
+    this.styled_cursor.style = STYLE_PASTE;
+    this.state = CopyPasteState.MOVING_PASTE;
+  }
+
+  /**
+   *
+   * @param {number} time
+   */
+  paste(time) {
+    // Select a random tile to copy
+    this.next_index = rand_index();
+    this.choose_next_target(time);
+    this.styled_cursor.style = STYLE_COPY;
+    this.state = CopyPasteState.MOVING_COPY;
+  }
+
+  /**
+   *
+   * @param {number} time
+   */
+  choose_next_target(time) {
+    const position = this.next_target;
+    this.cursor.position = position;
+
+    this.next_target = TILE_SIZE.mul_components(
+      new Direction(this.next_index.j, this.next_index.i),
+    ).to_point();
+
+    this.tween = Tween.point(
+      position,
+      this.next_target,
+      time,
+      DURATION_MOVE,
+      Ease.in_out_cubic,
+    );
+  }
+
+  /**
+   *
+   * @param {number} time
+   */
+  update(time) {
+    switch (this.state) {
+      case CopyPasteState.MOVING_COPY:
+        this.move(time);
+        return;
+      case CopyPasteState.COPY:
+        this.copy(time);
+        return;
+      case CopyPasteState.MOVING_PASTE:
+        this.move(time);
+        return;
+      case CopyPasteState.PASTE:
+        this.paste(time);
+        return;
+    }
+  }
+
+  draw(p) {
+    this.primitive.draw(p);
+  }
+}
 
 // @ts-ignore
 export const sketch = (p) => {
   let drawing_offscreen;
   let tilemap_copy;
   let tilemap_paste;
+  /**
+   * @type {CopyPaste | undefined}
+   */
+  let copy_paste;
 
   p.setup = () => {
     p.createCanvas(
@@ -82,20 +234,24 @@ export const sketch = (p) => {
       Point.ORIGIN,
     );
 
+    copy_paste = new CopyPaste(tilemap_copy, tilemap_paste);
+
     // set up a feedback loop - pasting to the screen impacts future copy
     // operations!
     SCENE_OFFSCREEN.regroup(tilemap_paste, ...SHAPES);
-    SCENE_SCREEN.regroup(SCENE_OFFSCREEN, STYLED_CURSOR, tilemap_copy);
+    SCENE_SCREEN.regroup(SCENE_OFFSCREEN, copy_paste, tilemap_copy);
   };
 
   p.draw = () => {
     p.background(0);
 
-    SCENE_SCREEN.draw(p);
-
     const time = CLOCK.elapsed_time;
     for (const shape of SHAPES) {
       shape.update(time);
     }
+
+    copy_paste?.update(time);
+
+    SCENE_SCREEN.draw(p);
   };
 };
