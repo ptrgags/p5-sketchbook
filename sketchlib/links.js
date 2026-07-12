54966fb9eaab4eb56cc23ed98475cbfb28a2871f
Peter Gagliardi <ptrgags@gmail.com>
2026-07-12 16:25:51 -0400

    Create empty sketch for ScreenScrambler

--- sketchlib/links.js
+++ sketchlib/links.js
@@ -1,6 +1,11 @@
 // Links for all of the sketches. Links are added to the page in this order
 export const LINKS = [
   {
+    title: "Screen Scrambler",
+    year: "2026-07",
+    is_lab: true,
+  },
+  {
     title: "Penrose Triangulation",
     year: "2026-07",
     is_lab: true,
