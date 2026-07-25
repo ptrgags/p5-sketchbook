0c882213dcf61469df4410629157dc66f963a5a0
Peter Gagliardi <ptrgags@gmail.com>
2026-07-25 12:10:12 -0400

    Fix PixelMaze image loading

--- PixelMaze/PixelMaze.js
+++ PixelMaze/PixelMaze.js
@@ -79,14 +79,18 @@ export const sketch = (p) => {
   };
 
   let current_sprite;
+  /**
+   * @type {P5Tilemap}
+   */
   let tilemap;
+  /**
+   * @type {Player}
+   */
   let player;
 
-  p.preload = () => {
-    preload_p5_resources(p, RESOURCE_MANIFEST, p5_resources);
-  };
+  p.setup = async () => {
+    await preload_p5_resources(p, RESOURCE_MANIFEST, p5_resources);
 
-  p.setup = () => {
     canvas = p.createCanvas(WIDTH, HEIGHT).elt;
 
     parse_resources(RESOURCE_MANIFEST, p5_resources, resources);
