27babad33dad1a0a6bf601b15915be839401888e
Peter Gagliardi <ptrgags@gmail.com>
2026-08-20 09:10:42 -0400

    Add to the list of sketches as lab sketch

--- sketchlib/links.js
+++ sketchlib/links.js
@@ -1,6 +1,11 @@
 // Links for all of the sketches. Links are added to the page in this order
 export const LINKS = [
   {
+    title: "DX7 Patch Viewer",
+    year: "2026-08",
+    is_lab: true,
+  },
+  {
     title: "Oklch Color Wheel",
     year: "2026-07",
     is_lab: true,
