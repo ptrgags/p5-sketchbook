f1f7caea0d03846619a00ab6dd82cbf414786ef0
Peter Gagliardi <ptrgags@gmail.com>
2026-08-26 05:44:53 -0400

    Add tests for dx7_checksum

--- sketchlib/dx7/dx7_checksum.js
+++ sketchlib/dx7/dx7_checksum.js
@@ -4,13 +4,13 @@
  * taking the two's complement (i.e. negative) and masking to 7 bits
  * (MIDI data bytes have a 0 in the 8th bit)
  *
- * @see {https://github.com/asb2m10/dexed/blob/2e182b3db85c09083ab13c8b9b00565ce7d9ff85/Source/PluginData.cpp#L30|Dexed implementation}
- * @see {https://homepages.abdn.ac.uk/d.j.benson/pages/dx7/sysex-format.txt|DX7 sysex format}
+ * @see {@link https://github.com/asb2m10/dexed/blob/2e182b3db85c09083ab13c8b9b00565ce7d9ff85/Source/PluginData.cpp#L30|Dexed implementation}
+ * @see {@link https://homepages.abdn.ac.uk/d.j.benson/pages/dx7/sysex-format.txt|DX7 sysex format}
  * @param {Uint8Array} bytes bytes to sum
  * @returns {number} The checksum
  */
 export function dx7_checksum(bytes) {
-  const sum = bytes.reduce((acc, x) => acc + x);
+  const sum = bytes.reduce((acc, x) => acc + x, 0);
   const twos_complement = -sum;
   return twos_complement & 0b1111111;
 }
